98e892210369dc0a7b7a3f1e56b843d0a0b4eff7
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-03 15:36:09 -0600

    GET/:id working

--- data/db-router.js
+++ data/db-router.js
@@ -17,11 +17,20 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
-    
-})
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts.findById(id)
+    .then(posts => {
+        posts ? res.status(200).json(posts) :
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+    .catch(error => {
+        console.log('error from GET/:id', error);
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    });
+});
 
-router.get('/', (req, res) => {
+router.get('/:id/comments', (req, res) => {
     
 })
 
