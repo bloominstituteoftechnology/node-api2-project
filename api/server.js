6104d934cdcd32d19741808e0a8d2aa6b4ab0e3e
Ale <alearanibar@gmail.com>
2019-12-03 22:30:27 -0800

    works

--- api/server.js
+++ api/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 
-const noteRoute = require('../new/newRoutes');
+const noteRoute = require("../new/newRoutes");
 const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send(`<h1>Class Project</h1>
-            <p>Welcome to the blog</p>`)
-})
+server.get("/", (req, res) => {
+  res.send(`<h1>Class Project</h1>
+            <p>Welcome to the blog</p>`);
+});
 
-server.use("/api/posts", noteRoute)
+server.use("/api/posts", noteRoute);
 
 module.exports = server;
--- new/newRoutes.js
+++ new/newRoutes.js
@@ -70,4 +70,136 @@ router.get('/:id/comments', (req, res) => {
     })
 })
 
+//POST	/api/posts	Creates a post using the information sent inside the request body.
+router.post ('/', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !content){
+        res
+            .status(400)
+            .json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Notes.insert(req.body)
+        .then(note => {
+            res
+                .status(201)
+                .json({note})
+        })
+        .catch(error => {
+            console.log(error)
+            res
+                .status(500)
+                .json({ error: "There was an error while saving the post to the database" })
+        })
+    }
+})
+// POST	/api/posts/:id/comments	Creates a comment for the post with the specified id using information sent inside of the request body.
+
+router.post('/:id/comments', (req, res) => {
+    const { text } = req.body
+    if (!text) {
+        res
+            .status(400)
+            .json({ errorMessage: "Please provide text for the comment." });
+    } else {
+        Notes.findById(req.params.id)
+        .then(note => {
+            if(note.length > 0){
+                Notes.insertComment(req.body)
+                .then(response =>{
+                    Notes.findCommentById(response.id)
+                    .then(newCom =>{
+                        res
+                        .status(201).
+                        json({newCom});
+                    })
+                })
+            }else{
+                res
+                    .status(404)
+                    .json({errorMessage: "Unable to find the post specified!"});
+            }
+        }).catch(error => {
+            res
+                .status(500)
+                .json({ error, message: "Unexpected error has occurred." });
+        });
+    }
+})
+
+router.delete('/api/posts/:id', (req,res) => {
+    Notes.findById(req.params.id)
+    .then(note => {
+        if(note.length > 0){
+            Notes.remove(req.params.id)
+            .then(gone => {
+                if(gone > 0){
+                res
+                .status(200)
+                .json({message: `Success ${gone} and ${req.params.id}`})
+                }
+            })
+        } else {
+            res
+                .status(404)
+                .json({ message: "The post with the specified ID does not exist." });
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res
+            .status(500)
+            .json({ error: "The post information could not be modified." })
+    })
+})
+// When the client makes a PUT request to /api/posts/:id:
+
+// If the post with the specified id is not found:
+
+// return HTTP status code 404 (Not Found).
+// return the following JSON object: { message: "The post with the specified ID does not exist." }.
+// If the request body is missing the title or contents property:
+
+// cancel the request.
+// respond with HTTP status code 400 (Bad Request).
+// return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.
+// If there's an error when updating the post:
+
+// cancel the request.
+// respond with HTTP status code 500.
+// return the following JSON object: { error: "The post information could not be modified." }.
+// If the post is found and the new information is valid:
+
+// update the post document in the database using the new information sent in the request body.
+// return HTTP status code 200 (OK).
+// return the newly updated post.
+
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body;
+
+    if (!title || !contents) {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+    } else {
+        Notes.update(req.params.id, req.body)
+        .then(note => {
+            if(note){
+            res
+                .status(200)
+                .json({post})
+            } else {
+                res
+                    .status(404)
+                    .json({  message: "The post with the specified ID does not exist." })
+            }
+            })
+        .catch(error => {
+            console.log(error)
+            res
+                .status(500)
+                .json({ error: "The post information could not be modified."})
+        })
+    }
+})
+
 module.exports=router
