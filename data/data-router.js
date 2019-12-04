6368477105803a04960c4b6b5c80e0748c2165de
Afia-Lydia <you@example.com>
2019-12-04 17:38:02 -0500

    updated code to include get for comments, worked on post

--- data/data-router.js
+++ data/data-router.js
@@ -64,12 +64,13 @@ router.delete("/:id", (req, res) => {
 
 	Data.remove(id)
 		.then(post => {
-			if (!post[0]) {
-				res
+			if (post) {
+				res.status(200).json({ message: "post deleted" });
+			} else {
+                
+                res
 					.status(404)
 					.json({ message: "The post with the specified ID does not exist. " });
-			} else {
-				res.status(200).json({ message: "post deleted" });
 			}
 		})
 		.catch(error => {
@@ -81,23 +82,63 @@ router.delete("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-    const changes = req.body;
-    Data.update(req.params.id, changes)
-      .then(post => {
-        if (post) {
-          res.status(200).json({message: "post updated", changes});
-        } else {
-          res.status(404).json({ message: "The post could not be found" });
-        }
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).json({
-          message: "Error updating the hub"
-        });
-      });
-  });
+	const changes = req.body;
+	Data.update(req.params.id, changes)
+		.then(post => {
+			if (post) {
+				res.status(200).json({ message: "post updated", changes });
+			} else {
+				res.status(404).json({ message: "The post could not be found" });
+			}
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(500).json({
+				message: "Error updating the hub"
+			});
+		});
+});
 
-  
+router.get("/:id/comments", (req, res) => {
+	const postID = req.params.id;
+
+	Data.findPostComments(postID)
+		.then(data => {
+			res.status(200).json(data);
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(500).json({ message: "error retrieving data" });
+		});
+});
+
+router.post("/:id/comments", (req, res) => {
+    const postID = req.params.id;
+    const { text } = req.body;
+	const postData = { text, postID };
+
+	if (!postData) {
+		res
+			.status(400)
+			.json({ errorMessage: "please provide text for the comment." });
+	} else {
+		Data.findById(postID).then(comment => {
+			if (comment) {
+				Data.insertComment(postData)
+					.then(message => {
+						res.status(201).json(message);
+					})
+					.catch(error => {
+						// log error to database
+						console.log(error);
+						res.status(500).json({
+							message: "Error adding the message",
+							id, postData
+						});
+					});
+			}
+		});
+	}
+});
 
 module.exports = router;
