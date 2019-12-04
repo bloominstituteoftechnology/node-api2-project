71e5216c071b398512fd1dff1ec89079656a590b
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-03 23:12:24 -0600

    add comments at /api/posts/:id/comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -17,16 +17,39 @@ router.post('/', (req, res) => {
       res.status(201).json(post)
     })
     .catch(error => {
-      console.log(error);
+      console.log('error on POST /api/posts', error);
       res.status(500).json({
-        message: 'There was an error while saving the post to the database'
+        errorMessage: 'There was an error while saving the post to the database'
       })
     })
   }
 })
 
 // Creates a comment for the post with specified id using information sent thru req.body
-router.post('/')
+router.post('/:id/comments', (req, res) => {
+  // const id = req.params.id;
+  const data = req.body;
+  console.log(data);
+  if (!data.text) {
+    res.status(400).json({ errorMessage: 'Please provide text for the comment.'})
+  } else {
+    db.insertComment(data)
+    .then(comment => {
+      console.log(comment);
+      if (comment) {
+        res.status(201).json(data)
+      } else {
+        res.status(404).json({ errorMessage: 'The post with the specified ID does not exist.' })
+      }
+    })
+    .catch(error => {
+      console.timeLog('error on POST /api/posts/:id/comments', error);
+      res.status(500).json({
+        errorMessage: 'There was an error while saving the comment to the database'
+      })
+    })
+  }
+})
 
 
 module.exports = router;
