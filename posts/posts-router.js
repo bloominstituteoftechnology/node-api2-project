afa31fad1996ac45444053feafc8fcb4f45d3dad
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-03 22:39:27 -0600

    add posts handles error for no title/contents

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -6,17 +6,27 @@ const db = require('../data/db.js');
 
 router.use(express.json());
 
+// Creates a post using the information sent thru req.body
 router.post('/', (req, res) => {
-  db.insert(req.body)
-  .then(post => {
-    res.status(201).json(post)
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({
-      message: 'Error adding to posts'
+  const data = req.body;
+  if (!data.title || !data.contents) {
+    res.status(400).json({ errorMessage: 'Please provide title and contents for the post.'})
+  } else {
+    db.insert(data)
+    .then(post => {
+      res.status(201).json(post)
     })
-  })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'There was an error while saving the post to the database'
+      })
+    })
+  }
 })
+
+// Creates a comment for the post with specified id using information sent thru req.body
+router.post('/')
+
 
 module.exports = router;
