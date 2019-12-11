ec6617c032037b24b6aed99a0cc8151d01a92cc6
Roger Lee <rmlee0003@gmail.com>
2019-12-10 19:01:12 -0500

    created get api/posts endpoint

--- data/hubs-router.js
+++ data/hubs-router.js
@@ -4,3 +4,16 @@ const router = express.Router()
 
 //get api/posts
 
+router.get("/", (req, res) => {
+ dbs
+   .find()
+   .then(posts => {
+     res.status(200).json(posts);
+   })
+   .catch(error => {
+     res.status(500).json({
+       error: "The posts information could not be retrieved"
+     });
+   });
+});
+
