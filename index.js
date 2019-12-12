fa27b5fc68aa7ebe94cd78e37abaa43f0b52aeac
Cole Gonzales <gcolegonzales003@gmail.com>
2019-12-12 14:49:34 -0600

    Added env host and port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use('/api/posts', postRouter)
 app.use('/api/posts/:id/comments', commentRouter)
 
+const host = process.env.HOST || "0.0.0.0"
 const port = process.env.PORT || 8080
 
 app.listen(port, () => {
