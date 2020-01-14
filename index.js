f8428ebe7871261a559621d5b155c91ed68e76af
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-14 13:52:58 +0000

    Initial request working

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require('express') 
-
 const app = express()
+const PORT = 6000;
+const hostname = '127.0.0.1';
 
-app.listen(6000, () => {
-    console.log('listening on 6000');
+const { 
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+    findPostComments,
+    findCommentById,
+    insertComment,} = require ('./data/db');
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json( "App runs");
+
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, hostname, () => {
+    console.log(`App running http://${hostname}:${PORT}`);
   })
