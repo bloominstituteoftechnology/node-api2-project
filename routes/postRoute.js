1d0f95648a09ba4a8b8606729242233566eb6df7
jordan <jordan@jordans-MacBook-Air.local>
2020-01-14 21:24:27 +0700

    having trouble with post requests on insomnia

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -18,4 +18,77 @@ router.get("/", (req, res) => {
     });
 });
 
+// grab posts by id
+router.get("/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then(post => {
+      if (post.length > 1) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "Post not found"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error retrieving the post"
+      });
+    });
+});
+
+//adding a post
+router.post("/", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      message: "Invalid post"
+    });
+  }
+
+  const newPost = {
+    title: req.body.title,
+    contents: req.body.contents
+  };
+
+  db.insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error adding the post"
+      });
+    });
+});
+
+//update posts
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      message: "Invalid post"
+    });
+  }
+
+  const update = {
+    title: req.body.title,
+    contents: req.body.contents
+  };
+
+  db.update(req.params.id, update)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post could not be found"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error updating post"
+      });
+    });
+});
+
 module.exports = router;
