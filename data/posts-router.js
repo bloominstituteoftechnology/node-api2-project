1697eb74f138835e55e5bb502b9d2ffc3d131bc2
Babatunde <babatundea15@gmail.com>
2020-01-14 17:47:43 +0100

    I worked on updating post endpoint

--- data/posts-router.js
+++ data/posts-router.js
@@ -82,5 +82,24 @@ router.delete(`/:id`, (req, res) => {
       });
     });
 });
+
+router.put(`/:id`, (req, res) => {
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: `The post could not be found`
+        });
+      }
+    })
+    .catch(error => {
+      // log error on the database
+      console.log(error);
+      res.status(500).json({ message: `Error updating the post` });
+    });
+});
 // export the router to the outside world
 module.exports = router;
