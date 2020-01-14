eeb2c3c95b0a1a446dca36ade9a1a2b0c30288bd
Babatunde <babatundea15@gmail.com>
2020-01-14 13:36:07 +0100

    I created the server and index files

--- data/db.js
+++ data/db.js
@@ -12,15 +12,15 @@ module.exports = {
   findCommentById,
   insertComment,
 };
-
+// GET posts /posts
 function find() {
   return db('posts');
 }
-
+// GET posts by id /post/:id 
 function findById(id) {
   return db('posts').where({ id: Number(id) });
 }
-
+// POST a post 
 function insert(post) {
   return db('posts')
     .insert(post, 'id')
@@ -32,27 +32,27 @@ function update(id, post) {
     .where('id', Number(id))
     .update(post);
 }
-
+// DELETE /posts/:id
 function remove(id) {
   return db('posts')
     .where('id', Number(id))
     .del();
 }
-
+// GET /posts/comments
 function findPostComments(postId) {
   return db('comments')
     .join('posts', 'posts.id', 'post_id')
     .select('comments.*', 'title as post')
     .where('post_id', postId);
 }
-
+// GET /posts/:id/comments
 function findCommentById(id) {
   return db('comments')
     .join('posts', 'posts.id', 'post_id')
     .select('comments.*', 'title as post')
     .where('comments.id', id);
 }
-
+// POST /posts/{payload}
 function insertComment(comment) {
   return db('comments')
     .insert(comment)
