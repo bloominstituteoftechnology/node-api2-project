00b92ff4d8ffb75028a2de006dd30971db495d3a
Robbie Porter <rthomporter@gmail.com>
2020-01-14 18:32:51 -0600

    Finalish

--- router.js
+++ router.js
@@ -111,8 +111,8 @@ router.delete("/:id", (req, res) => {
 
   DB.remove(id)
     .then(post => {
-      if (post.length !== 0) {
-        res.status(201).json(post);
+      if (post) {
+        res.status(200).json(post);
       } else {
         res.status(404).json({
           message: "The post with the specified ID does not exist."
@@ -128,16 +128,31 @@ router.delete("/:id", (req, res) => {
 });
 
 router.put("/:id/", (req, res) => {
-  DB.update(req.body)
-    .then(post => {
-      res.status(201).json(post);
-    })
-    .catch(err => {
-      console.log(err.message);
-      res.status(500).json({
-        error: "There was an error while saving the post to the database"
-      });
+  const { title, contents } = req.body;
+  const id = req.params.id;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
     });
+  } else {
+    DB.update(id, req.body)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err.message);
+        res.status(500).json({
+          error: "The post information could not be modified."
+        });
+      });
+  }
 });
 
 module.exports = router;
