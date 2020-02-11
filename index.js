0663b3e346f4ab26598047cea7f20cc784032d40
wesley.moody <wgm3190@gmail.com>
2020-02-11 16:35:41 -0600

    cra done added dotenv and cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const postRouter = require("./Routes/postRouter");
 const commentRouter = require("./Routes/commentRouter");
 const app = express();
 
 app.use(express.json()); // needed to parse JSON from the body
+app.use(cors())
 app.use("/api/posts", postRouter);
 app.use("/api/posts", commentRouter);
 
