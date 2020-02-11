591ca5ef2e2116f639b59cc665ce3f0f56aa5d41
Sunil Karki <karkisunil1200@gmail.com>
2020-02-11 15:48:42 -0600

    Getting post with Id works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -15,4 +15,16 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+  Hubs.findById(id)
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(err => {
+      console.log(err);
+      req.status(500).json({message: 'The post with the specified ID does not exist.'});
+    });
+});
+
 module.exports = router;
