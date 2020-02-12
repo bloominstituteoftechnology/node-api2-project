20b23f22e253a107b9af778f0f61406a94e8f803
Jonathan Hanson <jhanson8@uoregon.edu>
2020-02-12 13:01:45 -0800

    all operations

--- data/express-router.js
+++ data/express-router.js
@@ -80,4 +80,88 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
+// POST to /api/posts
+//how come it does not work if I .post to /api/posts?
+router.post("/", (req, res) => {
+  // axios.post(url, data, options); the data will be in body of the request
+  const hubInfo = req.body;
+  // validate the data, and if the data is valid save it
+  if (!hubInfo.title || !hubInfo.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+    return null;
+  }
+  Hubs.insert(hubInfo)
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+//POST to comments
+//if doesnt work
+router.post("/:id/comments", (req, res) => {
+  const { id } = req.params;
+
+  const comment = { ...req.body, post_id: id };
+
+  Hubs.insertComment(comment)
+    .then(inserted => {
+      res.status(201).json(inserted);
+    })
+    .catch(error => {
+      console.log(error);
+
+      res.status(500).json({ errorMessage: "sorry, we failed you" });
+    });
+});
+
+//PUT
+//add if statement for title and contents
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  Hubs.update(req.params.id, changes)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: "The hub could not be found" });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating the hub"
+      });
+    });
+});
+
+//DELETE
+router.delete("/:id", (req, res) => {
+  Hubs.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "The post has been deleted" });
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID could not be found"
+        });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error the post could not be removed"
+      });
+    });
+});
+
 module.exports = router;
