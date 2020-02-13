b1b7cae3a55b30e2211993a8e63241d8c03341da
Logan Sorensen <loganmsorensen@gmail.com>
2020-02-13 14:58:05 -0800

    made port dynamic

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ server.use(express.json());
 
 server.use("/api/posts", dbRouter);
 
-server.listen(5000, () => {
-  console.log("\n*** Server Running on http://localhost:5000 ***\n");
+server.get('/', (req, res) => {
+  res.send(`
+  <h2>Deployed API</h2>
+  `)
+})
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
