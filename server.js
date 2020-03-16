b8910f1a6e136912dd0874bca614f7a0fc0ea349
RoskiDeluge <roberto.delgado@gmail.com>
2020-03-15 16:06:26 -1000

    Set up postsRouter in server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
+const postsRouter = require('./posts/posts-router.js');
 
 const server = express();
 
-
 server.use(express.json());
+
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
     res.send(`
-        <h2>Lambda Hubs API</h>
-        <p>Welcome to the Lambda Hubs API</p>
+        <h2>RD's API</h2>
+        <p>Welcome to the Roberto's Blog Posts API</p>
       `);
   });
 
