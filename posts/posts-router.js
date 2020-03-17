79f177f37c08393c77c5e60d4ec7cfad8affe613
RoskiDeluge <roberto.delgado@gmail.com>
2020-03-16 20:32:01 -1000

    .get /:id working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -14,6 +14,21 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
 router.post('/', (req, res) => {
     const { title, contents } = req.body;
     !title || !contents 
