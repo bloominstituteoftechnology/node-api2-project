6b55504209c07e1e9ffd0b0cd5ba17178059b0b0
RoskiDeluge <roberto.delgado@gmail.com>
2020-03-17 16:21:16 -1000

    Changed .get to findPostComments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const Posts = require('../data/db.js');
 
 const router = express.Router();
@@ -29,13 +30,28 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The comments information could not be retrieved." })
+        })
+})
+
 router.post('/', (req, res) => {
     const { title, contents } = req.body;
     !title || !contents 
     ? res.status(400).json({ errorMessage: "Please provide title and content for post"})
     : Posts.insert(req.body)
-        .then(() => {
-            res.status(201).json(req.body);
+        .then((post) => {
+            res.status(201).json(post);
         })
         .catch(error => {
             // log error to database
@@ -47,11 +63,11 @@ router.post('/', (req, res) => {
 });
 
 router.post('/:id/comments', (req, res) => {
-    const { text } = req.body;
+    const { text }  = req.body;
     !text ?
     res.status(400).json({ errorMessage: "Please provide text for the comment." })
     : Posts.findById(req.params.id)
-        .then(post => {
+        .then((post) => {
             if (post) {
                 res.status(201).json(req.body)
             } else {
@@ -63,6 +79,8 @@ router.post('/:id/comments', (req, res) => {
             res.status(500).json({ error: "There was an error while saving the comment to the database" })
         })
 })
+
+
 
 
 module.exports = router;
