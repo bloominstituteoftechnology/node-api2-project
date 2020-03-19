53e2ed8831c72e3572ea40ef81aada96f08c5235
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-03-18 21:16:56 -0400

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const server = require('./api/server.js')
 
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () =>{
     console.log('\n *** Server is Running on http://localhost:${PORT} ***\n');
