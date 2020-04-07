47b5b0db0f91bfbc23928adbd54b3a7cecdad550
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-07 15:59:15 -0400

    GET /api/posts/:id.

--- posts/router.js
+++ posts/router.js
@@ -4,7 +4,7 @@ const router = express.Router(); // import Router with express
 
 // These will handle any request that begins with /api/posts:
 
-//	Returns an array of all the post objects contained in the database:
+// GET /api/posts... Returns an array of all the post objects contained in the database:
 router.get('/', (req, res) => {
     Posts.find(req.query)
     .then((posts) => {
@@ -13,6 +13,22 @@ router.get('/', (req, res) => {
     .catch((error) => {
         console.log(error);
         res.status(500).json({ message: "Error retrieving posts."})
+    })
+})
+
+// GET /api/posts/:id... Returns the post object with the specified id:
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then((post) => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "Post not found."})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "Error retrieving the post."})
     })
 })
 
