e68f6f25a3d82c730ece953f9abbb4b2a58511d8
anthony2698 <carrilloanthony2698@gmail.com>
2020-04-09 09:06:05 -0400

    Completed mvp.

--- Routes/routes.js
+++ Routes/routes.js
@@ -90,15 +90,75 @@ router.post('/', (req, res) => {
 });
 
 router.post('/:id/comments', (req, res) => {
- 
+    db.findById(req.params.id)
+
+    .then((post) => {
+        if (post) {
+            req.body.text ? 
+                db.insertComment(req.body)
+                    .then(res.status(200).json(req.body))
+                    .catch(err => {
+                        console.log(err);
+                    })
+            : res.status(400).json({ 
+                errorMessage: "Please provide text for the comment."
+            });
+        } else {
+            res.status(404).json({
+                message: "post not found",
+            });
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({
+            err,
+            message: "Error processing request",
+         });
+    });
 });
 
 router.put('/:id/comments', (req, res) => {
- 
+    if (req.body.title && req.body.contents) {
+		db.update(req.params.id, req.body)
+			.then((count) => {
+				if (count) {
+					db.findById(req.params.id).then((post) => res.status(200).json(post));
+				} else {
+					res.status(404).json({ 
+                        message: "The post with the specified ID does not exist" 
+                    });
+				}
+			})
+			.catch((err) =>
+				res.status(500).json({
+                    error: "The post information could not be modified" 
+                })
+			);
+	} else {
+		res.status(400).json({
+            message: "Please provide title and contents for the post" 
+        });
+	}
 });
 
 router.delete('/:id/comments', (req, res) => {
- 
+    db.remove(req.params.id)
+		.then((count) => {
+			if (count > 0) {
+				res.status(200).json({ 
+                    message: "The post has been deleted" 
+                });
+			} else {
+				res.status(404).json({ 
+                        message: "The post with the specified ID does not exist" 
+                    });
+			}
+		})
+		.catch((err) =>
+			res.status(500).json({ 
+                error: "The post could not be removed" 
+            })
+		);
 });
 
 
