fa2331aed58c6db14f093e1d31a8a387f761eb7c
Chris Giroux <cgiroux86@gmail.com>
2020-04-09 12:27:54 -0400

    staging

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const postsRouter = require("./routes/PostsRoutes");
 app.use(express.json());
 app.use("/api/posts", postsRouter);
 
-app.listen(4001, () => {
+app.listen(process.env.PORT || 4001, () => {
   console.log("listening on port 4001");
 });
