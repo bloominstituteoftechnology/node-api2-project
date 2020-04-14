f6f9ec160121b100fd4c8641e0a1fca38edd5a11
Your name <a.m.verdine@gmail.com>
2020-04-14 15:41:56 -0400

    Corrected import/export of database file/Write GET request and test with Insomnia

--- Posts/posts-routes.js
+++ Posts/posts-routes.js
@@ -3,4 +3,39 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  db.find(req.body)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "The posts information could not be retrieved.",
+      });
+    });
+});
+
+// MALANI GRACE TULLOCH
+
+// router.post("/", (req, res) => {
+
+//   if (!req.body.title || !req.body.contents) {
+//     return res.status(400).json({
+//       errorMessage: "Please provide title and contents for the post.",
+//     });
+//   } else if (req.body.title || req.body.contents) {
+//     db.insert(req.body)
+//       .then((post) => {
+//         res.status(201).json(post);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   } else {
+//     res.status(500).json({
+//       error: "There was an error while saving the post to the database",
+//     });
+//   }
+// });
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const postsRouter = require("./Posts/posts-routes");
+const db = require("./data/db");
 
 const server = express();
 
 server.use(express.json());
-server.use("/api/posts", postsRoutes);
+server.use("/api/posts", postsRouter);
 
 server.listen(8000, () => {
   console.log("Server has been intialized on port 8000...");
