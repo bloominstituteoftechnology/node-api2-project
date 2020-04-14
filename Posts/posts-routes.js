3cd4e2f62880a2f5fc65eb93d15d90885811ec31
Your name <a.m.verdine@gmail.com>
2020-04-14 19:00:30 -0400

    Create PUT request that updates post by postID/Successfully test in Insomnia

--- Posts/posts-routes.js
+++ Posts/posts-routes.js
@@ -81,4 +81,52 @@ router.post("/", (req, res) => {
   }
 });
 
+// router.post("/:id/comments", (req, res) => {
+//   if (!req.params.id) {
+//     res.status(404).json({
+//       message: "The post with the specified ID does not exist.",
+//     });
+//   } else if (!req.body.text) {
+//     return res.status(400).json({
+//       errorMessage: "Please provide text for the comment.",
+//     });
+//   } else if (req.params.id) {
+//     db.insertComment(req.body.text)
+//       .then((comment) => {
+//         res.status(201).json(comment);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   } else {
+//     return res.status(500).json({
+//       error: "There was an error while saving the comment to the database",
+//     });
+//   }
+// });
+
+router.put("/:id", (req, res) => {
+  if (!req.params.id) {
+    res.status(404).json({
+      message: "The post with the specified ID does not exist.",
+    });
+  } else if (!req.body.title || !req.body.contents) {
+    return escape.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  } else if (req.params.id || req.body.text || req.body.contents) {
+    db.update(req.params.id, req.body)
+      .then((post) => {
+        res.status(200).json(post);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.status(500).json({
+      error: "The post information could not be modified.",
+    });
+  }
+});
+
 module.exports = router;
