725085a93d938797d4d423d41467c85d46dab72b
Your name <a.m.verdine@gmail.com>
2020-04-14 10:27:40 -0400

    Create posts-routes file/Import post-routes into index.js

---
+++ Posts/posts-routes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const db = require("../data/db");
+
+const router = express.Router();
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const postsRouter = require("./Posts/posts-routes");
 
 const server = express();
+
+server.use(express.json());
+server.use("/api/posts", postsRoutes);
 
 server.listen(8000, () => {
   console.log("Server has been intialized on port 8000...");
