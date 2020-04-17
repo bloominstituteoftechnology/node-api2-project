0c3a1fdbf0d8d07a1f162384fa6a6be70de0ed4e
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-17 15:51:31 -0400

    API up and running

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+
+const server = express();
+const PORT = 8000;
+
+server.get('/', (req, res) => {
+  res.send('API up and running')
+});
+
+server.listen(PORT, () => {
+  console.log(`\n API running on PORT ${PORT}\n`)
+});
