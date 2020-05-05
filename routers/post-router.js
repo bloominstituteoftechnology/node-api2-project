90743eedca63971157b05887894b617e9603c0fd
Robert Elias <RobertElias@users.noreply.github.com>
2020-05-05 12:55:29 -0700

    created post-router.js

---
+++ routers/post-router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const Posts = require("../data/db.js"); // <fix the folder path></fix>
+
+const router = express.Router(); // mind the UPPERCASE R by visual studio code
+
+
+
+
+
+
+
+
+
+
+// mind the S in exportS
+module.exports = router; // same as below
+
+// export default server; // ES2015 MODULES
