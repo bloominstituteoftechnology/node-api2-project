32fe9b4cc964f4c3a5cdc422ab66d2a5282ad85e
thomas <t_huynh927@yahoo.com>
2020-05-08 21:38:35 -0700

    touched up on mvp

--- postsRouter.js
+++ postsRouter.js
@@ -4,20 +4,34 @@ const db = require("./data/db")
 
 const router = express.Router();
 
+function checkRequiredKeys(body, requiredKeys) {
+    for (const key of requiredKeys) {
+        if (!body[key]) {
+            return false
+        }
+    }
+    return true
+}
+
+function checkAllowedKeys(body, allowedKeys) {
+    allowedKeys = new Set(allowedKeys)
+    for (const key in body) {
+        if (!allowedKeys.has(key)) {
+            return false
+        }
+    }
+    return true
+}
+
 router.post('/', async (req, res) => {
     const requiredKeys = ['title', 'contents']
     try {
-        for (const key of requiredKeys) {
-            if (!req.body[key]) {
-                return res.status(400).send("Post needs both title and content")
-            }
-        }
+        !checkRequiredKeys(req.body, requiredKeys) && res.status(400).send("Post needs both title and content")
         const postId = await db.insert(req.body)
         res.status(201).send(postId)
     } catch(e) {
         res.status(500).send("Something happened! " + e)
     }
-
 })
 
 router.get('/', async (req, res) => {
@@ -49,12 +63,16 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
+    const allowedKeys = ['title', 'contents']
     try {
+        !checkAllowedKeys(req.body, allowedKeys) && res.status(400).json("Post edit needs either contents or title")
         const post = await db.update(req.params.id, req.body)
         post === 1 ? res.status(201).json(post) : res.status(404).json("post not found")
     } catch(e) {
         res.status(500).json("Something happened! " + e)
     }
 })
+
+
 
 module.exports = router;
