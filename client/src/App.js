10474b81e0366bbd7913c10c07dc919b14e30d6f
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-05-10 20:59:54 -0700

    and Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [post, setPost] = useState();
+
+  axios
+    .get('http://localhost:5000/api/posts')
+    .then(res => {
+      console.log(res);
+      setPost(res.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* {post.map(post => {
+        return <li>{post.content}</li>;
+      })} */}
     </div>
   );
 }
