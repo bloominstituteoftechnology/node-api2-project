51b1ffda48defb532b4ae41df1e4da59000015f7
anthony2698 <carrilloanthony2698@gmail.com>
2020-05-12 13:19:50 -0400

    Fixed port bug.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const server = require('./server')
 
 const port = process.env.PORT || 5000;
 
-server.listen(5000, () => {
+server.listen(port, () => {
     console.log(`\n Server running on port ${port}! \n`)
 });
