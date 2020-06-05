363785f5478c68d8d74650cb3e314cb4b6d65c4d
Marissa <marissacooterl@gmail.com>
2020-06-05 14:14:14 -0500

    POST /posts done

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -5,6 +5,27 @@ const posts = require("./posts-model");
 const router = express.Router();
 
 // POST /posts
+router.post("/posts", (req, res) => {
+    if (!req.body.title  || !req.body.contents) {
+        return res.status(400).json({
+            message: "Please provide title and contents for the post."
+        })
+    }
+    posts
+        .insert(req.body)
+        .then((post) => {
+            posts.findById(post.id)
+            .then((post) => {
+                res.status(201).json(post)
+            })
+        })
 
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({
+                message: "There was an error while saving the post to the database."
+            })
+        })
+})
 
 module.exports = router;
