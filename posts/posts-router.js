5f492e758795e020cf7b16d0fe5b34448aa56d58
Marissa <marissacooterl@gmail.com>
2020-06-05 17:51:55 -0500

    debugging POST /posts/:id/comments, moving on to GET /posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -19,11 +19,57 @@ router.post("/posts", (req, res) => {
                 res.status(201).json(post)
             })
         })
-
         .catch((error) => {
             console.log(error);
             res.status(500).json({
                 message: "There was an error while saving the post to the database."
+            })
+        })
+})
+
+// POST /posts/:id/comments
+// CHECK THIS LATER!!! 
+router.post("/posts/:id/comments", (req, res) => {
+    if (!posts.id) {
+        return res.status(404).json({
+            message: "The post with the specified ID does not exist."
+        })
+    }
+    if (!req.params.text) {
+        return res.status(400).json({
+            message: "Please provide text for the comment."
+        })
+    }
+    posts
+        .insertComment(req.body)
+        .then((comment) => {
+            posts.findCommentById(comment.id)
+            .then((comment) => {
+                res.status(201).json(post)
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "There was an error while saving the comment to the database."
+            })
+        })
+})
+
+// GET /api/posts
+router.get("/posts", (req, res) => {
+    posts
+        .find({
+            sortBy: req.query.sortBy,
+            limit: req.query.limit,
+        })
+        .then((posts) => {
+            res.status(200).json(posts);
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.status(500).json({
+                message: "The posts information could not be retrieved."
             })
         })
 })
