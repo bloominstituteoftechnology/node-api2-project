abc950d32801dc9fdc5c97b5b0a1c94c8fd31dab
Marissa <marissacooterl@gmail.com>
2020-06-05 13:07:07 -0500

    server debugged, moving on to building CRUD

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const posts = require("./posts-model");
+
+// stand alone router
+const router = express.Router();
+
+// POST /posts
+
+
+module.exports = router;
