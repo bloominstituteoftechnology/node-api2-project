69167eb94f48b7aa71f5f7471de1413519b31ff1
Cristina Altreche <c.altreche@gmail.com>
2020-08-04 18:43:03 -0400

    two post request complete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,36 +1,89 @@
-const express = require("express")
-const Posts = require("../data/db")
-const router  = express.Router()
+const express = require("express");
+const Posts = require("../data/db");
+const { restart } = require("nodemon");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).send('Hello from the GET/ posts endpoint')
-})
+//POST - API/POSTS
+//IF NO TITLE or CONTENTS THROW 404
+//ELSE POST USING INSERT
+router.post("/", (req, res) => {
+  const newPost = req.body;
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    res.status(200).send(`Hello from the GET/ posts /${id} endpoint`)
-})
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  } else {
+    Posts.insert(newPost)
+      .then((postData) => {
+        Posts.findById(postData.id).then((added) => {
+          res.status(201).json(added);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: "There was an error while saving the post to the database",
+        });
+      });
+  }
+});
 
-router.get('/:id/comments', (req, res) => {
-    const id = req.params.id
-    res.status(200).send(`hello from the GET /${id} /comments`)
-})
+//POST - API/POSTS/:id/COMMENTS
+//IF ID NOT FOUND THROW 404
+//IF TEXT IS MISSING THROW 400
+//IF VALID 201
+router.post("/:id/comments", (req, res) => {
+  const newComment = req.body;
 
-router.post('/', (req, res) => {
-    res.status(201).send('hello from the POST /posts endpoint')
-})
+  try {
+    if (!newComment.text) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Please provide text for the comment." });
+    } else {
+      newComment.post_id = req.params.id;
+      Posts.insertComment(newComment)
+        .then((comment) => {
+          Posts.findCommentById(comment.id).then((added) => {
+            res.status(201).json(added);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(404).json({
+            message: "The post with the specified ID does not exist.",
+          });
+        });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "There was an error while saving the comment to the database",
+    });
+  }
+});
 
-router.post('/:id/comments', (req, res) => {
-    const id = req.params.id
-    res.status(201).send(`hello from the POST /post /${id} /comments endpoint`)
-}) 
+router.get("/", (req, res) => {
+  res.status(200).send("Hello from the GET/ posts endpoint");
+});
 
-router.delete('/:id', (req, res) => {
-    res.status(204)
-})
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).send(`Hello from the GET/ posts /${id} endpoint`);
+});
 
-router.put('/:id', (req, res) => {
-    res.status(200).send(`hello from the PUT /posts /${id} `)
-})
+router.get("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  res.status(200).send(`hello from the GET /${id} /comments`);
+});
 
-module.exports = router
+router.delete("/:id", (req, res) => {
+  res.status(204);
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).send(`hello from the PUT /posts /${id} `);
+});
+
+module.exports = router;
