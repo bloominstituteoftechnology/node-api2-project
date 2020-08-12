320192d3d2ec25bd40abe5533753274ba3b304e6
Evelyn Nguyen <Evelyn@Viets-iMacx.local>
2020-08-11 17:53:16 -0700

    posts done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+
+const server = express();
+ 
+const port = 5000;
+
+server.use(express.json());
+server.use(postsRouter);
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
