2fa1be3456017fcd3d96e9cb0ac96b7ebcc54d19
Sean Kramer <seandominickramer@gmail.com>
2020-10-02 12:03:05 -0400

    Add endpoint for updating a post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,66 +1,85 @@
-const express = require('express');
-const posts = require('../data/db');
+const express = require("express");
+const posts = require("../data/db");
 const router = express.Router();
-
 
 //get posts
 router.get("/posts", (req, res) => {
   posts
-  .find(req.query)
-  .then((posts) => {
-    res.status(200).json(posts)
-  })
-  .catch((error) => {
-    res.status(500).json({
-      message: "Error retrieving posts"
+    .find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
     })
-  })
-}
-);
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error retrieving posts",
+      });
+    });
+});
 
-//get posts by is
+//get posts by id
 router.get("/posts/:id", (req, res) => {
   posts
-  .findById(req.params.id)
-  .then((post) => {
-    if(post) {
-      res.status(200).json(post)
-    } else {
-      res.status(404).json({
-        message: "Post not found",
-      });
-    }
-  })
-      .catch((error) => {
-        res.status(500).json({
-          message: "Error retrieving post",
+    .findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "Post not found",
         });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error retrieving post",
       });
-    
-   
-  
+    });
 });
 
 //create new post
 router.post("/posts", (req, res) => {
-  if(!req.body.title || !req.body.contents ) {
+  if (!req.body.title || !req.body.contents) {
     return res.status(400).json({
       message: "Missing title or contents",
     });
   }
 
-posts
-.insert(req.body)
-.then((post) => {
-  res.status(201).json(post);
-})
-.catch((error) => {
-  res.status(500).json({
-    message: "Error adding post",
-  });
+  posts
+    .insert(req.body)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error adding post",
+      });
+    });
 });
 
-});
+//update post
+router.put("/posts/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      message: "Missing title or content",
+    });
+  }
 
+  posts
+    .update(req.params.id, req.body)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "Post not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error updating post",
+      });
+    });
+});
 
 module.exports = router;
