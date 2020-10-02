b0b16c9ca76bf5c82832c22d48bce15fc7fe1a9e
Sean Kramer <seandominickramer@gmail.com>
2020-10-02 11:27:47 -0400

    Add endpoint for getting posts by it

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const posts = require('../data/db');
 const router = express.Router();
 
+
+//get posts
 router.get("/posts", (req, res) => {
-  
   posts
   .find(req.query)
   .then((posts) => {
@@ -15,7 +16,31 @@ router.get("/posts", (req, res) => {
     })
   })
 }
-)
+);
+
+//get posts by is
+router.get("/posts/:id", (req, res) => {
+  posts
+  .findById(req.params.id)
+  .then((post) => {
+    if(post) {
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({
+        message: "Post not found",
+      });
+    }
+  })
+      .catch((error) => {
+        res.status(500).json({
+          message: "Error retrieving post",
+        });
+      });
+    
+   
+  
+});
+
 
 
 module.exports = router;
