c4a3f6f9be7f147b22f8994b07b23840e6f41232
Emmanuel Padilla <manolin923@gmail.com>
2020-10-28 08:09:05 -0700

    working on PUT

--- data/posts-router.js
+++ data/posts-router.js
@@ -23,10 +23,11 @@ postRouter.get("/api/posts/:id", (req, res) => {
   posts
     .findById(req.params.id)
     .then((post) => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
+      console.log(post.title);
+      if (!post) {
         res.status(404).json({ message: "Post not found" });
+      } else {
+        res.status(200).json(post);
       }
     })
     .catch((error) => {
@@ -34,6 +35,7 @@ postRouter.get("/api/posts/:id", (req, res) => {
       console.log(error);
       res.status(500).json({
         message: "Error retrieving the post",
+        error: error.message,
       });
     });
 });
@@ -41,9 +43,9 @@ postRouter.get("/api/posts/:id", (req, res) => {
 postRouter.get("/api/posts/:id/comments", (req, res) => {
   posts
     .findPostComments(req.params.id)
-    .then((post) => {
-      if (post) {
-        res.status(200).json(post);
+    .then((data) => {
+      if (data) {
+        res.status(200).json(data);
       } else {
         res.status(404).json({ message: "Post not found" });
       }
@@ -52,38 +54,69 @@ postRouter.get("/api/posts/:id/comments", (req, res) => {
       // log error to database
       console.log(error);
       res.status(500).json({
-        message: "Error retrieving the post",
+        message: error.message,
       });
     });
 });
 
 //--------------------------POST----------------------------------------------//
 
-// postRouter("/api/posts", (req, res) => {
-//   const newPost = { id: req.params.id, ...req.body }
-//   posts.insert(newPost)
-//   .then(data => {
-//     res.status(201).json(data)
-//   })
-//   .catch(error => {
-//     console.log(error.message, error.stack)
-//     res.status(500).json({
-//       message: error.message,
-//       stack: error.stack,
-//     })
-//   })
-// })
+postRouter.post("/api/posts", (req, res) => {
+  const newPost = { id: req.params.id, ...req.body };
+  posts
+    .insert(newPost)
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch((error) => {
+      console.log(error.message, error.stack);
+      res.status(500).json({
+        message: error.message,
+        stack: error.stack,
+      });
+    });
+});
 
-// postRouter("/api/posts/:id/comments", (req, res) =>{
-//   const newComment = { id: req.params.id, ...req.body }
-//   posts.insertComment(newComment)
-//   .then(data => {
-//     res.status(201).json(data)
-//   })
-
-// })
+postRouter.post("/api/posts/:id/comments", (req, res) => {
+  const newComment = { post_id: req.params.id, ...req.body };
+  posts
+    .insertComment(newComment)
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch((error) => {
+      console.log(error.message, error.stack);
+      res.status(500).json({
+        message: error.message,
+        stack: error.stack,
+      });
+    });
+});
 
 //--------------------------DELETE----------------------------------------------//
+
+postRouter.delete("/api/posts/:id", (req, res) => {
+  posts
+    .remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "the post has been removed" });
+      } else {
+        res.status(404).json({ message: "the post cannot be found" });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the hub",
+      });
+    });
+});
+
+postRouter.delete("/api/posts/:id");
+  posts
+
 
 //--------------------------PUT----------------------------------------------//
 
