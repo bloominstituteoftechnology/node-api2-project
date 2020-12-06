9b2e1a57d73358fbe085e078327d4019fe2646a0
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-12-06 15:12:12 -0600

    server up and running

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const express = require('express')
+const posts = require('./posts/posts-router')
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+
+server.get("/", (req,res) => {
+    res.json({
+        message: "Welcome To My Posts API!"
+    })
+})
+server.listen(port, () => {
+    console.log(`server running on ${port}`)
+})
