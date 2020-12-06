7f6d30edb182577cae91a9375c84226af7e4263f
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-12-06 15:31:06 -0600

    get for id working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -16,4 +16,21 @@ router.get('/',(req,res) => {
     })
 })
 
+router.get('/:id', (req,res) => {
+    posts.findById(req.params.id)
+    .then((posts) => {
+        if(posts) {
+            res.status(200).json(posts)
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    })
+})
 module.exports = router
