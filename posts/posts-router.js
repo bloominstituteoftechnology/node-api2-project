83e82047009dc0d19ae4bd3063271fd1f60c6c67
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-12-06 15:43:09 -0600

    put working on id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -46,9 +46,33 @@ router.get('/:id/comments', (req,res) => {
         }
     })
     .catch(() =>{
-
+        res.status(500).json({
+            message: "The comments could not be found"
+        })
     })
 })
 
+router.put('/:id', (req,res) => {
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }
 
+    posts.update(req.params.id, req.body)
+    .then((posts) => {
+        if(posts) {
+            res.status(200).json(posts)
+        } else {
+            res.status(404).json({
+                message: "Post with the ID does not exist"
+            })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({
+            message: "The post information could not be modified"
+        })
+    })
+})
 module.exports = router
