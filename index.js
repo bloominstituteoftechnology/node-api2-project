f59895634c1b132b3c37db032bbb0da574dc3d53
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-12-10 20:00:23 -0600

    added posts route and get enpoint for server

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const server = express()
 const port = 9000
 
 server.use(express.json())
+server.use("/posts", postsRouter)
+
+server.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to our API",
+    })
+})
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
