d2a48e30747f384162cc641e5c8743f9f5cf0ec5
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-17 10:41:25 -0500

    all posts api's created

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router();
 
 const Post = require('../db-helpers');
+const { post } = require('../server');
 
+const errorMessage = 'The post with the specficied id does not exist'
 
 // GET posts
 router.get('/api/posts', async (req,res)=>{
@@ -22,7 +24,7 @@ router.get('/api/posts/:id',async(req,res)=>{
         if(post){
             res.status(200).json(post)
         }else{
-            res.status(404).json({error:'The post with the specific ID does not exist.'})
+            res.status(404).json({error:errorMessage})
         }
     }catch{
         res.status(500).json({error:'the post information could not be retrieved'})
@@ -35,10 +37,54 @@ router.delete('/api/posts/:id',async(req,res)=>{
     try{
         const user = await Post.remove();
         if(!user){
-            res.status(404).json({error:'the post with the specified id does not exist.'})
+            res.status(404).json({error:errorMessage})
         }
     }catch{
-        user.abort();
+        abort();
         res.status(500).json({error:'the post could not be removed.'})
     }
 })
+
+//CREATE POST
+
+router.post('/api/posts',async(req,res)=>{
+    const post = req.body;
+    if(!post.title || !post.contents){
+        abort()
+        res.status(400).json({error:'Please provide title and contents for the post'})
+    }else{
+        try{
+            const newPost = await Post.insert(post);
+            res.status(201).json(newPost);
+        }catch{
+            res.status(500).json({error:'there was an error while saving the post to the database.'})
+        }
+    }
+})
+
+// UPDATE POST
+router.put('/api/posts/:id', async(req,res)=>{
+    const {id} = req.params;
+    const changes = req.body;
+    
+    if(!post.id){
+        res.status(404).json({error:errorMessage})
+    }else{
+        try{
+            if(!post.title || !post.contents){
+                abort()
+                res.status(400).json({error:'Please provide title and contents for the post'})
+            }else{
+                const updatedPost = Post.update(id,changes)
+                if(updatedPost){
+                    res.status(200).json(updatedPost)
+                }
+            }
+        }catch{
+            abort();
+            res.status(500).json({error:'the post information could not be modified.'})
+        }
+    }
+})
+
+module.exports = router;
