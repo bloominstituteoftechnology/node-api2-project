bb4f400b3b175c23351091188dd70d0ffa052bdd
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-17 10:15:02 -0500

    get specific post id info completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,11 +3,28 @@ const router = express.Router();
 
 const Post = require('../db-helpers');
 
+
+// GET posts
 router.get('/api/posts', async (req,res)=>{
     const posts = await Post.find();
     try{
-        res.status(201).json(posts);
+        res.status(200).json(posts);
     }catch{
         res.status(500).json({error:'The posts information could not be retrieved.'})
+    }
+})
+
+//get specific post
+router.get('/api/posts/:id',async(req,res)=>{
+    const {id} = req.params;
+    const post = await Post.findById(id);
+    try{
+        if(post){
+            res.status(200).json(post)
+        }else{
+            res.status(404).json({error:'The post with the specific ID does not exist.'})
+        }
+    }catch{
+        res.status(500).json({error:'the post information could not be retrieved'})
     }
 })
