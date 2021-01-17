a637c867525891af83ca59504a4944253560c2c9
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2021-01-16 18:28:09 -0800

    endpoints written

--- api/lotr-router.js
+++ api/lotr-router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const Hobbit = require('./db-helpers.js')
+const Hobbit = require('./db-helpers.js');
+const { count } = require('console');
 
 //Endpoints
 
@@ -46,8 +47,58 @@ router.get('/api/posts/:id/comments', (req, res) => {
 });
 
 
+router.post('/api/posts', (req, res) => {
+    Hobbit.insert(req.body)
+    .then(hobbit => {
+        res.status(201).json(hobbit);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The comments information could not be retrieved."
+        });
+    });
+})
 
-// | GET    | /api/posts              | Returns an array of all the post objects contained in the database.     (Done)                                                                                                    |
-// | GET    | /api/posts/:id          | Returns the post object with the specified id.   (Done)      
-                                                                                                                     
-// | GET    | /api/posts/:id/comments | Returns an array of all the comment objects associated with the post with the specified id.   (Done)
+
+
+router.post('/api/posts/:id/comments', (req, res) => {
+    Hobbit.insertComment(req.body)
+    .then(hobbit => {
+        res.status(201).json(hobbit);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The comments information could not be retrieved."
+        });
+    });
+})
+
+router.delete('/api/posts/:id', (req,res) => {
+    Hobbit.remove(req.params.id)
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({message: 'Deleted'})
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post could not be removed" });
+    });
+})
+
+
+router.put('/api/posts/:id', (req, res) => {
+    const changes = req.body
+    Hobbit.update(req.params.id, changes)
+    .then(hobbit => {
+        if(hobbit) {
+            res.status(200).json(hobbit);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The comments information could not be retrieved."
+        });
+    });
+
+})
