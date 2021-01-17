ede0c063533222df3b6a2f9e6ab3817a86bfac68
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-17 17:42:18 -0500

    fixed issues

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,39 +1,120 @@
+const e = require('express');
 const express = require('express');
-const { restart } = require('nodemon');
 const router = express.Router();
-
 const DB = require('../db-helpers');
-const { post, route } = require('../server');
 
-const errorMessage = 'The post with the specficied id does not exist'
+const errorMessage = 'The post with the specified id does not exist'
 
-// POST 
-router.post("/", (req,res)=>{
-    const newPost = req.body;
+//GET /api/posts
 
-    // Ensure that the title or contents are specified
-  if (
-    newPost.title === undefined ||
-    newPost.title === "" ||
-    newPost.contents === undefined ||
-    newPost.contents === ""
-  ) {
-    res.status(400).json({
-      errorMessage: "Please provide title and contents for the post.",
-    });
-    return;
-  }
+router.get('/',async (req,res)=>{
+    const posts = await DB.find();
+    try{
+        res.status(200).json(posts)
+    }catch{
+        res.status(500).json({error:'the posts information could not be retrieved.'})
+    }
+})
 
-  DB.insert(newPost)
-    .then((post) => {
-      res.status(201).json({ ...post, ...newPost });
+//GET /api/posts/:id
+
+router.get('/:id',(req,res)=>{
+    const {id} = req.params;
+
+    DB.findById(id)
+    .then((post)=>{
+        if(post[0]=== undefined){
+            res.status(404).json({error:errorMessage})
+        }else{
+            res.status(200).json(post)
+        }
     })
-    .catch(() => {
-      res.status(500).json({
-        error: "There was an error while saving the post to the database",
-      });
-    });
-});
+    .catch(()=>{
+        res.status(500).json({error:'The post information could not be retrieved'})
+    })
+})
+
+// GET /api/posts/:id/comments 
+
+router.get('/:id/comments',(req,res)=>{
+    const{id}= req.params;
+
+    DB.findById(id)
+    .then((post)=>{
+        if(post[0]=== undefined){
+            res.status(404).json({error:errorMessage})
+        }else{
+            DB.findPostComments(id)
+            .then((comments)=>{
+                res.status(200).json(comments);
+            })
+            .catch((err)=>{
+                res.status(500).json({error:"The comments information could not be retrieved"})
+            })
+        }
+    })
+    .catch((err)=>{
+        res.status(500).json(err.message)
+    })
+})
+
+
+//DELETE /posts/:id
+
+router.delete('/:id',(req,res)=>{
+    const {id} = req.params;
+    DB.findById(id)
+    .then((post)=>{
+        if(post[0]=== undefined){
+            res.status(404).json({error:errorMessage})
+        }else{
+            DB.remove(id)
+            .then((post)=>{
+                res.status(200).json({message:"Post deleted"})
+            })
+            .catch((err)=>{
+                res.status(500).json({error:"The post could not be removed"})
+            })
+        }
+    })
+    .catch(()=>{
+        res.status(500).json({error:'The post information could not be retrieved'})
+    })
+})
+
+//update the post api/posts/:id
+// change it JSON when updating you idiot
+
+router.put('/:id',(req,res)=>{
+    const {id} = req.params;
+    const updatedPost = req.body;
+    DB.findById(id)
+    .then((post)=>{
+        if(post[0]=== undefined){
+            res.status(404).json({error:errorMessage})
+        }else{
+            if(!updatedPost.title || !updatedPost.contents){
+                res.status(400).json({error:"please provide the title and contents for the post"});
+                return;
+            }
+
+            DB.update(id,updatedPost)
+                .then((post)=>{
+                    res.status(200).json({...post[0],...updatedPost})
+                })
+                .catch(()=>{
+                    res.status(500).json({error:"the post information could not be modified"})
+                })
+        }
+    })
+    .catch(()=>{
+        res.status(500).json({error:'The post information could not be retrieved'})
+    })
+})
+
+
+
+
 
 
 module.exports = router;
