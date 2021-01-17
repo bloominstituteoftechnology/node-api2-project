15a721718792a6d5e9de031b9c968f7045813758
KVlearn <kavya.mdu@gmail.com>
2021-01-16 17:30:02 -0800

    api working

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express=require('express');
-
+var cors = require('cors');
 const server=express();
 //declare and bring in the new router
 const postsRouter=require('../node-api2-project/api/posts-router')
 const commentsRouter=require('../node-api2-project/api/comments-router');
 //use a middleware - postsRouter by having '/api/posts' as base url
 server.use(express.json());
+server.use(cors());
 server.use('/api/posts',postsRouter);
 server.use('/api/posts',commentsRouter); //for get comments
   
