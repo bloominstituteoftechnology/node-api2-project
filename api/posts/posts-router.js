d7950392e831e90e2db443a3a8b154214e16d243
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-17 16:36:58 -0500

    trying to reset

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,90 +1,39 @@
-const express = require('express')
+const express = require('express');
+const { restart } = require('nodemon');
 const router = express.Router();
 
-const Post = require('../db-helpers');
-const { post } = require('../server');
+const DB = require('../db-helpers');
+const { post, route } = require('../server');
 
 const errorMessage = 'The post with the specficied id does not exist'
 
-// GET posts
-router.get('/', async (req,res)=>{
-    const posts = await Post.find();
-    try{
-        res.status(200).json(posts);
-    }catch{
-        res.status(500).json({error:'The posts information could not be retrieved.'})
-    }
-})
+// POST 
+router.post("/", (req,res)=>{
+    const newPost = req.body;
 
-//get specific post
-router.get('/:id',async(req,res)=>{
-    const {id} = req.params;
-    const post = await Post.findById(id);
-    try{
-        if(post){
-            res.status(200).json(post)
-        }else{
-            res.status(404).json({error:errorMessage})
-        }
-    }catch{
-        res.status(500).json({error:'the post information could not be retrieved'})
-    }
-})
+    // Ensure that the title or contents are specified
+  if (
+    newPost.title === undefined ||
+    newPost.title === "" ||
+    newPost.contents === undefined ||
+    newPost.contents === ""
+  ) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+    return;
+  }
 
-//DELETE SPECIFIC POST 
-router.delete('/:id',async(req,res)=>{
-    const {id} = req.params;
-    try{
-        const user = await Post.remove();
-        if(!user){
-            res.status(404).json({error:errorMessage})
-        }
-    }catch{
-        abort();
-        res.status(500).json({error:'the post could not be removed.'})
-    }
-})
+  DB.insert(newPost)
+    .then((post) => {
+      res.status(201).json({ ...post, ...newPost });
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+      });
+    });
+});
 
-//CREATE POST
-
-router.post('/',async(req,res)=>{
-    const post = req.body;
-    if(!post.title || !post.contents){
-        abort()
-        res.status(400).json({error:'Please provide title and contents for the post'})
-    }else{
-        try{
-            const newPost = await Post.insert(post);
-            res.status(201).json(newPost);
-        }catch{
-            res.status(500).json({error:'there was an error while saving the post to the database.'})
-        }
-    }
-})
-
-// UPDATE POST
-router.put('/:id', async(req,res)=>{
-    const {id} = req.params;
-    const changes = req.body;
-    
-    if(!post.id){
-        res.status(404).json({error:errorMessage})
-    }else{
-        try{
-            if(!post.title || !post.contents){
-                abort()
-                res.status(400).json({error:'Please provide title and contents for the post'})
-            }else{
-                const updatedPost = Post.update(id,changes)
-                if(updatedPost){
-                    res.status(200).json(updatedPost)
-                }
-            }
-        }catch{
-            abort();
-            res.status(500).json({error:'the post information could not be modified.'})
-        }
-    }
-})
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const server = express();
+const postRouter = require('./posts/posts-router');
 
 server.use(express.json())
+server.use('/api/posts',postRouter);
 
 
 
