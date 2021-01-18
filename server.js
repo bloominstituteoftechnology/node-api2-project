2c23b6cfdbbf20ede1100c2fb41c3890d10d00cf
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-01-17 23:59:58 -0800

    Figuring out what the issues are here

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const postsRouter = require('./api/posts/posts-router.js');
+const server = express();
+
+
+server.use(express.json()); 
+
+
+server.use('/api/posts', postsRouter);
+// server.use(server.router);
+// OTHER ENDPOINTS 
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Posts API</h>
+    <p>Welcome and here is the API for get</p>
+  `);
+});
+// postsRouter.initialize(server);
+// we expose the server
+module.exports = server
