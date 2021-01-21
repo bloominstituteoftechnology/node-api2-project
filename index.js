47c1396424ef456336d10aef7421217bf2a07c3e
KVlearn <kavya.mdu@gmail.com>
2021-01-21 08:45:06 -0800

    updated npm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server=require('../node-api2-project/server');
 
-PORT= process.env.PORT;
+PORT= process.env.PORT  || 5000;
 server.listen(PORT,()=>{
     console.log(`Server listening in port ${PORT}`)
 })
