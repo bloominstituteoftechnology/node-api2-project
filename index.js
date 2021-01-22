7eecca8d01f0ea101c0d1cacf39cbc00c1059868
KVlearn <kavya.mdu@gmail.com>
2021-01-21 17:55:07 -0800

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  const server=require('./api/server');
 
-const PORT= process.env.PORT || 5000;
-server.listen(PORT,()=>{
-    console.log(`Server listening port${PORT}`)
+const port= process.env.PORT || 5000;
+server.listen(port,()=>{
+    console.log(`Server listening port${port}`)
 })
