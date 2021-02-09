7e9482a0264c379a07ca371017a3cd2c69f418a7
Michelle Chen <mchen0062@gmail.com>
2021-02-09 14:33:23 -0800

    completed full functionality

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express'); 
+
+const myServer = express(); 
+const postsRouter = require('./posts/posts-router');
+
+myServer.use(express.json()); 
+myServer.use('/api/posts', postsRouter); 
+
+myServer.get('/', (req, res) => {
+    res.send(`<h1>Hello there, from myServer in server.js</h1>`);
+})
+
+module.exports = myServer;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+const server = require('./api/server'); 
+port = 3000; 
+
+server.listen(3000, () => {
+    console.log(`Server running on port ${port}`)
+});
