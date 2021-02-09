f747b554c2fe44e1ea6854ae9ffa419beef18b1a
Diego Roman <diegormnv209@gmail.com>
2021-02-09 15:11:15 -0800

    need to fix bug in post for it to add object to array

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,11 +1,11 @@
 // implement your posts router here
-const Post = require('./posts-model');
+const Posts = require('./posts-model');
 const express = require('express');
 const router = express.Router();
 
 //Gets all posts
 router.get('/', (req, res) =>{
-    Post.find()
+    Posts.find()
     .then((posts) =>{
         res.status(200).json(posts);
     })
@@ -18,11 +18,31 @@ router.get('/', (req, res) =>{
 //Gets post at specified id
 router.get('/:id', (req, res) =>{
     const { id } = req.params;
-    Post.findById(id)
+    Posts.findById(id)
     .then((user) =>{
-        user ? res.status(200).json(user) : res.status(404).json({message: 'The post with the specified ID does not exist'})
+        user ? res.status(200).json(user) : res.status(404).json({message: 'The post with the specified ID does not exist'});
     })
-})
+    .catch((err) =>{
+        res.status(500).json({message: 'The post information could not be retrieved'});
+    });
+});
+
+//Post new object to array(creates new object)
+router.post('/', (req, res) =>{
+    const newPost = req.body;
+    
+    if(!newPost.title || !newPost.contents){
+        res.status(400).json({message: 'Please provide title and contents for the post'});
+    } else{
+        Posts.insert(newPost)
+        .then((post) =>{
+            res.status(201).json(post);
+        })
+        .catch((error) =>{
+            res.status(500).json({message: 'There was an error while saving the post to the database'});
+        });
+    }
+});
 
 
 module.exports = router;
