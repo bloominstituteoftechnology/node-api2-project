209175eeed9ad1765b9204e9050697170ca2c3f8
Jacob Stephens <jacobstephens2@gmail.com>
2021-02-09 18:46:45 -0800

    finished

--- api/server.js
+++ api/server.js
@@ -1,2 +1,21 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const server = express();
+const postsRouter = require('./posts/posts-router');
+
+
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Server Test</h>
+      <p>Success!</p>
+    `);
+  });
+
+
+  
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+const server = require('./api/server')
+const PORT = 4000;
+
+
+server.listen(PORT, () => {
+console.log(`Server running on: http://localhost:${PORT}/`)});
