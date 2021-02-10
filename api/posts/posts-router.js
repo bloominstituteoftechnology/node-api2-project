d3890f38609e65c141c2aca533c720a05abb3d6d
Diego Roman <diegormnv209@gmail.com>
2021-02-09 16:46:13 -0800

    put is still buggy

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -45,26 +45,51 @@ router.post('/', (req, res) =>{
 });
 
 //PUT will update post with specified id
-router.put('/:id', (req, res) =>{
-    const id = req.params.id;
-    const changes = req.body;
+// router.put('/:id', (req, res) =>{
+//     const id = req.params.id;
+//     const changes = req.body;
 
-    try{
-        if(!changes.title || !changes.contents){
-            res.status(404).json({message: 'The post with the specified ID does not exist'})
-        } else{
-            Posts.update(id, changes)
-            .then((post) =>{
-                res.status(200).json({post});
-            })
-            .catch((error) =>{
-                res.status(400).json({message: 'Please provide title and contents for the post'});
-            });
-        }
-    } catch(error){
-        res.status(500).json({message: 'The post information could not be modified'});
+//         if(!changes.title || !changes.contents){
+//             res.status(400).json({message: 'Please provide title and contents for the post'});
+//         } else{
+//             Posts.update(id, changes)
+//             .then((post) =>{
+//                 if(!post){
+//                     res.status(404).json({message: 'The post with the specified ID does not exist'});
+//                 } else{
+//                     res.status(200).json({post});
+//                 }  
+//             })
+//             .catch((error) =>{
+//                 res.status(500).json({message: 'The post information could not be modified'});
+//             });
+//         }
+// });
+
+router.put('/:id', (req, res) => {
+    const targetId = req.params.id;
+    const changes = req.body;
+  
+    try {
+      if (!changes.title || !changes.contents) {
+        res
+          .status(400)
+          .json({ message: 'Please provide title and contents for the post' });
+      } else {
+        Posts.update(targetId, changes).then((id) => {
+          id
+            ? Post.findById(targetId).then((post) => res.status(200).json(post))
+            : res.status(404).json({
+                message: 'The post with the specified ID does not exist',
+              });
+        });
+      }
+    } catch (e) {
+      res
+        .status(500)
+        .json({ message: 'The post information could not be modified' });
     }
-});
+  });
 
 //DELETE post with specified id
 router.delete('/:id', (req,res) =>{
