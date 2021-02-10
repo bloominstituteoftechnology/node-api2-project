59f8dc87c5fd400eb6fa7d68cd376a67de6df7e2
Devon Brady <devon.a.brady@gmail.com>
2021-02-09 19:36:51 -0500

    added server using routes

--- api/server.js
+++ api/server.js
@@ -1,2 +1,8 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postRouter = require('./posts/posts-router.js');
+const server = express();
+server.use(express.json());
+server.use("/api/posts",postRouter )
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // require your server and launch it here
+const server = require('./api/server');
+server.listen(1234, ()=>{
+    console.log('\n *** Server is running on http://localhost:1234 ***\n')
+});
