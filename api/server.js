a0f585b49943243228d85b674660eb2151e1eb3b
ty <tylerpmatthews1998@gmail.com>
2021-02-09 22:46:20 -0500

    some errors

--- api/server.js
+++ api/server.js
@@ -1,2 +1,8 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const post = require('./posts/posts-router');
+const server = express();
+server.use(express.json());
+server.use('/api/posts', post)
+module.exports = server
