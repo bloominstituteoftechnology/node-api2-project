398d3d4f701ca0de8ce5cf0e5a97cfd597230de4
IsaacGHoward <howardisaac100@gmail.com>
2021-02-12 19:06:24 -0800

    GET post by ID working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,8 +10,22 @@ router.get('/', (req,res) => {
   .then((response) => {
     res.send(response);
   })
-  .err(() => {
+  .catch(() => {
     res.status(500).send({ message: "The posts information could not be retrieved" });
+  })
+})
+
+router.get('/:id', (req,res) => {
+  //console.log(req.params.id);
+  db.findById(req.params.id)
+  .then((response) => {
+    if(response)
+      res.send(response);
+    else 
+      res.status(404).send({ message: "The post with the specified ID does not exist" });
+  })
+  .catch(() => {
+    res.status(500).send({ message: "The post information could not be retrieved" });
   })
 })
 
