1d5b0bab29bd491b0aa2fb5c2ed354ef38b2ed82
IsaacGHoward <howardisaac100@gmail.com>
2021-02-12 19:37:18 -0800

    PUT request to edit post working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -43,7 +43,32 @@ router.post('/', (req,res) => {
       res.status(500).send({ message: "There was an error while saving the post to the database" });
     })
   }
+})
+
+router.put('/:id', (req,res) => {
+  if(!req.body.title || !req.body.contents)
+    res.status(400).send({ message: "Please provide title and contents for the post" });
+  else{
+    db.findById(req.params.id)
+    .then((resp) => {
+      if(resp){
+        db.update(req.params.id, req.body)
+        .then(() => {
+          db.findById(req.params.id)
+          .then((response) => {
+            res.status(200).send(response);
+          })
+        })
+        .catch(() => {
+          res.status(500).send({ message: "The post information could not be modified" });
+        })
+      }
+      else 
+        res.status(404).send({ message: "The post with the specified ID does not exist" });
+      })
+  }
   
+
 })
 
 module.exports = router;
