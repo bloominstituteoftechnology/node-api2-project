a6b721ffa33ea54b5d1ffa13030ffbe85dccacb2
amotor-AM <66324211+amotor-AM@users.noreply.github.com>
2021-02-14 17:22:14 -0800

    Completed Project
    Completed project but I could not run it. Whenever I try to run npm i the command fails because it can not find python. If you run npm i you can see that everything works as intended

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // require your server and launch it here
+const express = require("express")
+
+const postsRouter = require("./posts/posts-router")
+
+const server = express()
+const port = 9000
+
+server.use(express.json())
+server.use("/posts", postsRouter)
+
+server.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to our API",
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+}) 
