8438f970711c4d8539f70706c01d6733da6f7054
CaseyBlatz <caseyblatz@yahoo.com>
2021-03-09 17:11:14 -0700

    MVP

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+const http = require("http")
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+
+    //tell the browser to render the response as HTML
+    res.setHeader("Content-Type", "text/html")
+
+    //send data
+    res.write("<h1>Hello, World</h1>")
+    res.end()
+})
+
+module.exports = server;
