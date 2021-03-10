3c082bf8fbc3151ad21d6aaf28191460ecd2de1b
blackcatwizard <55123268+bigbootygoblins@users.noreply.github.com>
2021-03-09 16:48:10 -0800

    mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,60 @@
 // implement your posts router here
+const Posts = require('./posts-model');
+const express = require('express');
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Posts.find()
+        .then((posts) => {
+            res.status(200).json(posts)
+        })
+        .catch((err) => {
+            res.status(500).json({ message: `${err.message}` })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+
+    Posts.findById(id)
+        .then((posts) => {
+            if (!posts) {
+                res.status(404).json({ message: `post with id ${id} does not exist` })
+            } else {
+                res.status(200).json(posts)
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ message: `get error: ${err}` })
+        })
+})
+
+router.post('/', async (req, res) => {
+    const post = await Posts.insert(req.body)
+    if (!post) {
+        res.status(400).json({ message: `${post} not found` })
+    } else {
+        res.status(201).json(post)
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Posts.remove(id)
+        .then((posts) => {
+            res.status(200).json(posts)
+        })
+        .catch((err) => {
+            res.status(500).json({ message: `${err}` })
+        })
+})
+
+router.get('/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+        .then((posts) => {
+            res.status(200).json(posts)
+        })
+        .catch((err) => {res.status(500).json({ message: `${err}` })})
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,7 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+const postsRouter = require('./post/posts-router')
+const server = express()
+server.use(postsRouter)
+module.exports = server
