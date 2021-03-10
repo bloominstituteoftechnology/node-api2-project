860e232efdbadac4eced1cfbb73b4100fb6f9010
bria barry <briabarry@iMac.lan>
2021-03-09 19:37:29 -0800

    MVP

--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+
+const postRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postRouter);
+
+module.exports = server;
