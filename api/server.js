61eddc803b4d15338f7df4b2da3db6cb38def325
alex-wallander <alexwallander@gmail.com>
2021-03-11 15:28:05 -0600

    test

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 // implement your server here
 // require your posts router and connect it here
 
+require('dotenv').config();
+
+const path = require('path')
 const express = require('express');
 
 const postsRouter = require('./posts/posts-router');
@@ -8,7 +11,17 @@ const postsRouter = require('./posts/posts-router');
 const server = express();
 
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'api/build')))
+
+if(process.env.NODE_ENV !== 'development') {
+    const cors = require('cors');
+    server.use(cors());
+}
+server.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'api/build', 'index.html'))
+})
 
 server.use('/api/posts', postsRouter)
+
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // require your server and launch it here
+require('dotenv').config()
 
 const server = require('./api/server')
 
-server.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
