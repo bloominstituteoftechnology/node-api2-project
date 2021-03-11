6ed12f88f6a554058de8aad545581e195bb5f0d8
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-03-10 16:30:27 -0800

    GET post comments done & mvp met

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -76,6 +76,21 @@ router.delete('/api/posts/:id', (req,res)=>{
 })
 
 
+router.get('/api/posts/:id/comments', (req, res)=> {
+    const {id} = req.params;
 
+    Posts.findPostComments(id)
+        .then(comments => {
+            if (!comments) {
+                res.status(404).json({ message: "The post with the specified ID does not exist"})
+            } else {
+                res.status(200).json(comments)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: "The comments information could not be retrieved"})
+        })
+
+})
 
 module.exports = router;
