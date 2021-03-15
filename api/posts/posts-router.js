f6cb19ae9651e7e129dcd355d7ad74c531932f16
Avery Burns <a.aburns95@gmail.com>
2021-03-15 02:03:48 -0500

    added put method needs delete and comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -80,4 +80,32 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    if (!changes.title || !changes.contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.update(id, changes)
+            .then((post) => {
+                if (post) {
+                    res.status(200).json(post);
+                } else {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                res.status(500).json({
+                    message: "The post information could not be modified"
+                })
+            })
+    }
+})
+
 module.exports = router;
