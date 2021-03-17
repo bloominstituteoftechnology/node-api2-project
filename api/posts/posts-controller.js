055ef34dd3fbff01b17ec75a240c7c1e5746b76c
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-03-16 23:58:55 -0600

    not done

---
+++ api/posts/posts-controller.js
@@ -0,0 +1,25 @@
+
+
+
+const Posts = require('./posts-model.js');
+
+
+
+
+module.exports = {
+//GET
+    getPosts:async function (req, res) {
+        try {
+            const posts = await Posts.find(req.query);
+            res.status(200).json(posts)
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: 'error retrieving posts'
+            });
+
+    }
+}
+
+
+
--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,25 @@
 // implement your posts router here
+const express = require('express');
+const router = express.Router();
+
+const Posts = require('./posts-model.js')
+
+
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+      .then(post => {
+        res.status(200).json(post);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'post info not retrieved',
+        });
+      });
+  });
+
+ 
+ 
+
+
+  module.exports = router;
