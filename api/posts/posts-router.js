f7f2b38d4554fb57ab3a69dac5f3c5b20d577556
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-03-17 01:42:32 -0600

    have all endpoints done but not getting errors through postman HELP!

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -35,8 +35,71 @@ router.get('/', (req, res) => {
       });
   });
 
- 
+  router.post('/', (req, res) => {
+    Posts.insert(req.body)
+      .then(posts => {
+        res.status(201).json(posts);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(400).json({
+          message: 'Please provide title and contents for the post',
+        });
+      });
+  });
  
 
+  router.put('/:id', (req, res) => {
+    const changes = req.body;
+    Posts.update(req.params.id, changes)
+      .then(posts => {
+        if (posts) {
+          res.status(200).json(posts);
+        } else {
+          res.status(404).json({ message: 'message: "The post with the specified ID does not exist' });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error updating the post',
+        });
+      });
+  });
+
+  router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({ message: 'The post has been nuked' });
+        } else {
+          res.status(404).json({ message: 'The post could not be found' });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error removing the post',
+        });
+      });
+  });
+
+
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+      .then(comments => {
+        if (comments.length > 0) {
+          res.status(200).json(dogs);
+        } else {
+          res.status(404).json({ message: 'No comments for this post' });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error retrieving the dogs for this adopter',
+        });
+      });
+  });
 
   module.exports = router;
