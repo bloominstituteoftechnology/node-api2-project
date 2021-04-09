5bf30dc5bd7202159b68d1560a9994b42326547e
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-09 13:06:21 -0600

    finished get request for post comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -103,7 +103,22 @@ router.delete("/api/posts/:id", (req, res) => {
 })
 
 router.get("/api/posts/:id/comments", (req, res) => {
-    
+    if(!req.params.id){
+        res.status(404).json({
+            message: "The post with the specified ID does not exist"
+        })
+    }
+
+    posts.findPostComments(req.params.id)
+        .then(comments => {
+            res.status(200).json(comments)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "The comments information could not be retrieved"
+            })
+        })
 })
 
 module.exports = router
