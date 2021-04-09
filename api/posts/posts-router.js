d08359eca7abbd2f949bb1184fd8c85b09ac710d
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-08 20:07:01 -0600

    wrote endpoints 1 and 2

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -6,11 +6,35 @@ const posts = require("./posts-model")
 const router = express.Router()
 
 router.get("/api/posts", (req, res) => {
-
+    posts.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            })
+        })
 })
 
 router.get("/api/posts/:id", (req, res) => {
-    
+    posts.findById(req.params.id)
+        .then(post => {
+            if(post){
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "The post information could not be retrieved"
+            })
+        })
 })
 
 router.post("/api/posts	", (req, res) => {
