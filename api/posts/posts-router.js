06b8aa9526545f52bfd7b3872c9c2564d0d078f9
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-09 12:41:57 -0600

    finished delete request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -82,7 +82,24 @@ router.put("/api/posts/:id", (req, res) => {
 })
 
 router.delete("/api/posts/:id", (req, res) => {
-    
+    if(!req.params.id){
+        res.status(404).json({
+            message: "The post with the specified ID does not exist"
+        })
+    }
+
+    posts.remove(req.params.id)
+        .then(post => {
+            res.status(200).json({
+                message: "The post has been deleted"
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "The post could not be removed"
+            })
+        })
 })
 
 router.get("/api/posts/:id/comments", (req, res) => {
