17889effbb290b063ece74313b27f094ebf76b3d
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-09 12:09:49 -0600

    finished post and put request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -37,19 +37,55 @@ router.get("/api/posts/:id", (req, res) => {
         })
 })
 
-router.post("/api/posts	", (req, res) => {
+router.post("/api/posts", (req, res) => {
+    if(!req.body.title || !req.body.contents){
+        return res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }
+
+    posts.insert(req.body)
+            .then(post => {
+                res.status(201).json({post})
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database"
+                })
+            })
+})
+
+router.put("/api/posts/:id", (req, res) => {
+    if(!req.params.id){
+        res.status(404).json({
+            message: "The post with the specified ID does not exist"
+        })
+    } 
+    
+    posts.update(req.params.id, req.body)
+        .then(post => {
+            if(!req.body.title || !req.body.contents){
+                res.status(400).json({
+                    message: "Please provide title and contents for the post"
+                })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "The post information could not be modified"
+            })
+        })
+})
+
+router.delete("/api/posts/:id", (req, res) => {
     
 })
 
-router.put("/api/posts/:id	", (req, res) => {
-    
-})
-
-router.delete("/api/posts/:id	", (req, res) => {
-    
-})
-
-router.get("/api/posts/:id/comments	", (req, res) => {
+router.get("/api/posts/:id/comments", (req, res) => {
     
 })
 
