40006aceb085a8b2702a4a0e8fedc27b20c38938
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-08 19:55:37 -0600

    required and launched server. installed npm and express. implemented server and connected posts router. laid out code for posts-router.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,32 @@
 // implement your posts router here
+
+const express = require("express")
+const posts = require("./posts-model")
+
+const router = express.Router()
+
+router.get("/api/posts", (req, res) => {
+
+})
+
+router.get("/api/posts/:id", (req, res) => {
+    
+})
+
+router.post("/api/posts	", (req, res) => {
+    
+})
+
+router.put("/api/posts/:id	", (req, res) => {
+    
+})
+
+router.delete("/api/posts/:id	", (req, res) => {
+    
+})
+
+router.get("/api/posts/:id/comments	", (req, res) => {
+    
+})
+
+module.exports = router
