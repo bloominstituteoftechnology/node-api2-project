8c022740a68c6842358d254ba4cc219b11ebf14c
Tony So <60905365+TonySo88@users.noreply.github.com>
2021-04-11 14:08:27 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express")
+const postsRouter = require("./posts/posts-router")
+
+const server = express()
+
+server.use(express.json())
+server.use(postsRouter)
+ 
+module.exports = server
