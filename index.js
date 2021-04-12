ed4cad58c125cb5d8a96edda1b322bd41182473f
River <rivyyriv@gmail.com>
2021-04-12 15:27:08 -0600

    Endpoints Routed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // require your server and launch it here
+
+const express = require('express');
+
+const postRoutes = require("./api/posts/posts-router.js");
+
+const server = express();
+
+
+server.use('/api/posts', postRoutes);
+
+
+server.listen(8000, () => console.log('API running on port 8000'));
