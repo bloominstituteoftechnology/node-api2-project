7d7298d4f992d5718b81267b577b087ee456d278
hans0 <kiessler.hans@gmail.com>
2021-05-04 15:53:20 -0400

    Initial commit, got first server running

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,12 @@
 // implement your posts router here
+let router = require('express').Router()
+const Posts = require('./posts-model');
+
+router.get('/', (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+});
+
+module.exports = router;
