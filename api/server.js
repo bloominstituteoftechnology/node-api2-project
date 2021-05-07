ada5eb9a2fa9984386bffc7a9359f8be24b6ff66
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-05-07 09:59:18 -0400

    mvp done

--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+const postsRouter = require("./posts/posts-router");
+
+const server = express();
+
+server.use(express.json());
+server.use(postsRouter);
+
+server.get("/", (req, res) => {
+	res.send(`
+    <h2>Lambda Posts API</h>
+    <p>Welcome to the Lambda Posts API</p>
+  `);
+});
+
+// common.js equiv of default export
+module.exports = server;
