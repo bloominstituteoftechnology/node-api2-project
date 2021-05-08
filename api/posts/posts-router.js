f724bebe1ad1e195d7b4f6143268a4a9e6acdc88
Mitchell Walker <eagleye1144@gmail.com>
2021-05-08 17:22:35 -0600

    3

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,24 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/', (req, res)=>{
+    res.json('foo')
+
+})
+router.get('/:id', (req, res)=>{
+
+})
+router.post('/', (req, res)=>{
+
+})
+router.delete('/:id', (req, res)=>{
+
+})
+router.put('/:id', (req, res)=>{
+
+})
+router.get('/:id/messages', (req, res)=>{
+
+})
+
 module.exports = router
