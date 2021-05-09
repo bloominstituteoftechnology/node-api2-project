1e36da8cb4bffb8e73f22e1005e5497d712e1434
Mitchell Walker <eagleye1144@gmail.com>
2021-05-08 18:31:43 -0600

    done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -121,8 +121,25 @@ router.put('/:id', (req, res)=>{
         })
     }
 })
-router.get('/:id/messages', (req, res)=>{
-
+router.get('/:id/comments', async (req, res)=>{
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+    } else{ 
+        const messages = await Post.findPostComments(req.params.id)
+        res.json(messages)
+    }
+}
+        catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 
 module.exports = router
