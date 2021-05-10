a8ede20a011c87eb76f4327ea734f760b82eb7ba
Ryan Clark <rbclark244@gmail.com>
2021-05-09 23:38:37 -0400

    start

--- api/server.js
+++ api/server.js
@@ -1,2 +1,7 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+const User = require("../api/posts/posts-router");
+const server = express();
+
+server.use(express.json());
