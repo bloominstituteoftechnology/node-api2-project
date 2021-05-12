5fe07087aac6f55c16d7e4c484dad0442df094c4
Ryan Clark <rbclark244@gmail.com>
2021-05-11 23:05:47 -0400

    fully built but untested

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require("express");
-const User = require("../api/posts/posts-router");
+const postsRouter = require("../api/posts/posts-router");
+const apiPostsRouter = require("../api/posts");
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/posts/posts-router", postsRouter);
+
+server.use("/api/posts", apiPostsRouter);
+
+module.exports = server;
