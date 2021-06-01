76277424e799dbdd707accfda3f928758ea5adf2
Cooper Jackson <davisjaxon@gmail.com>
2021-05-31 17:18:28 -0700

    finished assignment

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+const postRouter = require('./posts/posts-router')
+
+server.use(express.json())
+
+server.use('/api/posts', postRouter)
+
+server.get('/', (req, res) => {
+    res.status(404).send(`What's going on boys.`)
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require('./api/server')
+
+server.listen(8000, () => {
+    console.log('Listening to Port 8000')
+})
