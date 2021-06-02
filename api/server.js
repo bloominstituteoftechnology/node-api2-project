239fdb6014ad8264307f2895b21ab944d31c345f
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-01 20:15:22 -0400

    set up router for posts

--- api/server.js
+++ api/server.js
@@ -4,4 +4,15 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+//Router Imports
+const postsRouter = require("./posts/posts-router");
+
+server.use("/api/posts", postsRouter);
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
+
 module.exports = server;
