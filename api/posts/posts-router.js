79526721550c8435bebc5322d3b53f2ae7bd1d96
Corie Stewart <coriestewart1989>
2021-06-04 18:02:00 -0400

    working on it

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,25 @@
 // implement your posts router here
+const express = require("express");
+
+const router = express.Router();
+
+const Posts = require("./posts-model")
+
+//Posts endpoints
+
+router.get("/", (req, res) => {
+    Posts.find(req.body)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving the posts"
+        })
+    })
+})
+
+router.get("/:id", (req,res))
+
+module.exports = router
