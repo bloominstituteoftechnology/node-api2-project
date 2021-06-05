3cb029a92e28bbab5d9ca4284005e61268a0c933
artofmayhem <chef_tony_miller@yahoo.com>
2021-06-04 16:11:14 -1000

    commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// require your server and launch it here
+const server = require('./api/server');
+
+const port = 5000;
+const message = "Server Running: localhost:5000"
+// START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(message)
+})
