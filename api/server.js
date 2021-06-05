5bae13054243782133f4bd2239c22b738e63d082
miketillman95 <matillman95@gmail.com>
2021-06-05 00:24:04 -0700

    adding-comments

--- api/server.js
+++ api/server.js
@@ -1,4 +1,16 @@
 // implement your server here
-    
+    const express = require('express');
 
+    const server = express ();
+
+    server.use(express.json());
+
+
+
+//  create a const that requires the post model
 // require your posts router and connect it here
+server.post ('/api/posts', (req, res) => {
+
+
+
+})
