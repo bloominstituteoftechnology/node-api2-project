bc8f7daef643f7e0d58f37584a94f1f022cc38c9
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-06 15:47:15 +0200

    retouch tests

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -10,12 +10,11 @@ beforeEach(async () => {
   await db('comments').truncate()
   await db('posts').truncate()
 })
-afterAll(async (done) => {
+afterAll(async () => {
   await db.destroy()
-  done()
 })
 
-it('sanity check', () => {
+test('[0] sanity check', () => {
   expect(true).not.toBe(false)
 })
 
@@ -30,8 +29,8 @@ const post2 = {
 }
 
 describe('server.js', () => {
-  describe('1 [GET] /api/posts', () => {
-    it('can get the correct number of posts', async () => {
+  describe('[GET] /api/posts', () => {
+    test('[1] can get the correct number of posts', async () => {
       let res = await request(server).get('/api/posts')
       expect(res.body).toHaveLength(0)
       await db('posts').insert(post1)
@@ -41,7 +40,7 @@ describe('server.js', () => {
       res = await request(server).get('/api/posts')
       expect(res.body).toHaveLength(2)
     }, 500)
-    it('can get all the correct posts', async () => {
+    test('[2] can get all the correct posts', async () => {
       await db('posts').insert(post1)
       await db('posts').insert(post2)
       let res = await request(server).get('/api/posts')
@@ -51,8 +50,8 @@ describe('server.js', () => {
       expect(res.body[1]).toMatchObject(post2)
     }, 500)
   })
-  describe('2 [GET] /api/posts/:id', () => {
-    it('can get all the correct posts by id', async () => {
+  describe('[GET] /api/posts/:id', () => {
+    test('[3] can get all the correct posts by id', async () => {
       await db('posts').insert(post1)
       await db('posts').insert(post2)
       let res = await request(server).get('/api/posts/1')
@@ -62,18 +61,18 @@ describe('server.js', () => {
       expect(res.body).toHaveProperty('id')
       expect(res.body).toMatchObject(post2)
     }, 500)
-    it('reponds with a 404 if the post is not found', async () => {
+    test('[4] reponds with a 404 if the post is not found', async () => {
       let res = await request(server).get('/api/posts/111')
       expect(res.status).toBe(404)
       expect(res.body.message).toMatch(/does not exist/i)
     }, 500)
   })
-  describe('3 [POST] /api/posts', () => {
-    it('responds with a 201', async () => {
+  describe('[POST] /api/posts', () => {
+    test('[5] responds with a 201', async () => {
       const res = await request(server).post('/api/posts').send(post1)
       expect(res.status).toBe(201)
     }, 500)
-    it('responds with a new post', async () => {
+    test('[6] responds with a new post', async () => {
       let res = await request(server).post('/api/posts').send(post1)
       expect(res.body).toHaveProperty('id')
       expect(res.body).toMatchObject(post1)
@@ -81,34 +80,34 @@ describe('server.js', () => {
       expect(res.body).toHaveProperty('id')
       expect(res.body).toMatchObject(post2)
     }, 500)
-    it('on missing title or contents responds with a 400', async () => {
+    test('[7] on missing title or contents responds with a 400', async () => {
       let res = await request(server).post('/api/posts').send({ title: 'foo' })
       expect(res.status).toBe(400)
       res = await request(server).post('/api/posts').send({ contents: 'bar' })
       expect(res.status).toBe(400)
     }, 500)
   })
-  describe('4 [PUT] /api/posts/:id', () => {
-    it('responds with updated user', async () => {
+  describe('[PUT] /api/posts/:id', () => {
+    test('[8] responds with updated user', async () => {
       const [id] = await db('posts').insert(post1)
       const updates = { title: 'xxx', contents: 'yyy' }
       const res = await request(server).put(`/api/posts/${id}`).send(updates)
       expect(res.body).toMatchObject({ id, ...updates })
     }, 500)
-    it('saves the updated user to the db', async () => {
+    test('[9] saves the updated user to the db', async () => {
       const [id] = await db('posts').insert(post2)
       const updates = { title: 'aaa', contents: 'bbb' }
       await request(server).put(`/api/posts/${id}`).send(updates)
       let user = await db('posts').where({ id }).first()
       expect(user).toMatchObject({ id, ...updates })
     }, 500)
-    it('responds with the correct message & status code on bad id', async () => {
+    test('[10] responds with the correct message & status code on bad id', async () => {
       const updates = { title: 'xxx', contents: 'yyy' }
       const res = await request(server).put('/api/posts/foobar').send(updates)
       expect(res.status).toBe(404)
       expect(res.body.message).toMatch(/does not exist/i)
     }, 500)
-    it('responds with the correct message & status code on validation problem', async () => {
+    test('[11] responds with the correct message & status code on validation problem', async () => {
       const [id] = await db('posts').insert(post2)
       let updates = { title: 'xxx' }
       let res = await request(server).put(`/api/posts/${id}`).send(updates)
@@ -124,19 +123,19 @@ describe('server.js', () => {
       expect(res.body.message).toMatch(/provide title and contents/i)
     }, 500)
   })
-  describe('5 [DELETE] /api/posts/:id', () => {
-    it('reponds with a 404 if the post is not found', async () => {
+  describe('[DELETE] /api/posts/:id', () => {
+    test('[12] reponds with a 404 if the post is not found', async () => {
       let res = await request(server).delete('/api/posts/111')
       expect(res.status).toBe(404)
       expect(res.body.message).toMatch(/does not exist/i)
     }, 500)
-    it('reponds with the complete deleted post', async () => {
+    test('[13] reponds with the complete deleted post', async () => {
       await db('posts').insert(post1)
       let res = await request(server).delete('/api/posts/1')
       expect(res.body).toHaveProperty('id')
       expect(res.body).toMatchObject(post1)
     }, 500)
-    it('removes the deleted post from the database', async () => {
+    test('[14] removes the deleted post from the database', async () => {
       const [id] = await db('posts').insert(post2)
       let post = await db('posts').where({ id }).first()
       expect(post).toBeTruthy()
@@ -145,13 +144,13 @@ describe('server.js', () => {
       expect(post).toBeFalsy()
     }, 500)
   })
-  describe('6 [GET] /api/posts/:id/comments', () => {
-    it('reponds with a 404 if the post is not found', async () => {
+  describe('[GET] /api/posts/:id/comments', () => {
+    test('[15] reponds with a 404 if the post is not found', async () => {
       let res = await request(server).get('/api/posts/66/comments')
       expect(res.status).toBe(404)
       expect(res.body.message).toMatch(/does not exist/i)
     }, 500)
-    it('can get all the comments associated to the posts with given id', async () => {
+    test('[16] can get all the comments associated to the posts with given id', async () => {
       await db('posts').insert(post1)
       await db('posts').insert(post2)
       const comments = [
