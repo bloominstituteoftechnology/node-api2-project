01165ac26f43e9bc0f784ff9bcd566284bd7a1d5
Amos Martineau <amos.martineau@gmail.com>
2021-07-08 21:11:01 -0600

    MVP complete

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+const PostsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+server.use(PostsRouter);
+
+server.get('/', (req, res) => {
+  const welcomeMsg = process.env.WMSG || 'Welcome to my API server!'
+  res.status(200).json({ welcomeMsg })
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it here
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
