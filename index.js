410d7a540b2c0105ea7836a879d8678dc620f978
Stan T <stan@Stans-Mac-mini.local>
2021-07-10 17:59:53 -0700

    Initial project setup

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+server.listen(3000, () => {
+  console.log("\n*** Server running on http://localhost:3000 ***\n");
+});
