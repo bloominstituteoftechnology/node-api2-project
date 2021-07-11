3759f1612a55b58e52c23fcd37f56745cf59b609
Stan T <stan@Stans-Mac-mini.local>
2021-07-10 18:45:46 -0700

    express install, server setup

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,21 @@
 // implement your posts router here
+const express = require("express");
+const Post = require("./posts-model");
+
+const router = express.Router();
+
+router.get("/api/posts", (req, res) => {
+  Post.find()
+    .then((posts) => {
+      console.log(posts);
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+      });
+    });
+});
+
+module.exports = router;
