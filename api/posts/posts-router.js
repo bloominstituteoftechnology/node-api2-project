47179e2322286068cea360c953161bf65d62cd8d
Stan T <stan@Stans-Mac-mini.local>
2021-07-10 19:39:40 -0700

    GET posts by :id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 ✓ [1] can get the correct number of posts
 ✓ [2] can get all the correct posts */
 router.get("/", (req, res) => {
-  Post.find()
+  Post.find(req.query)
     .then((posts) => {
       console.log(posts);
       res.status(200).json(posts);
@@ -17,6 +17,24 @@ router.get("/", (req, res) => {
       res.status(500).json({
         message: "The posts information could not be retrieved",
       });
+    });
+});
+
+/* GET] /api/posts/:id
+✓ [3] can get all the correct posts by id
+✓ [4] reponds with a 404 if the post is not found */
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post does not exist" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Error retrieving the post" });
     });
 });
 
