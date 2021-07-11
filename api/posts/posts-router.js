ed0585aeb666bd3c2861c73d5b7ad7e98f9b42c6
Stan T <stan@Stans-Mac-mini.local>
2021-07-10 23:22:02 -0700

    PUT - bug -8 fixed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -84,35 +84,39 @@ router.post("/", (req, res) => {
 ✓ [10] responds with the correct message & status code on bad id
 ✓ [11] responds with the correct message & status code on validation problem 
 */
-router.put("/:id", async (req, res) => {
-  try {
-    const possiblePost = await Post.findById(req.params.id);
-    if (!possiblePost) {
-      res.status(404).json({
-        message: "The post with the specified ID does not exist",
-      });
-    } else {
-      if (!req.body.title || !req.body.contents) {
-        res.status(400).json({
-          message: "Please provide title and contents for the post",
-        });
-      } else {
-        const updatePost = await Post.update(req.params.id, req.body);
-        res.status(200).json(updatePost);
-      }
-    }
-  } catch (err) {
-    res.status(500).json({
-      message: "The post information could not be modified",
-      err: err.message,
-      stack: err.stack,
+
+router.put("/:id", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post",
     });
-    const post = await Post.update(req.params.id, req.body);
-    // if (!post) {
-    //   res.status(400).json({ message: `user ID does not exist` });
-    // } else {
-    res.status(200).json(post);
-    // }
+  } else {
+    Post.findById(req.params.id)
+      .then((posts) => {
+        if (!posts) {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist",
+          });
+        } else {
+          return Post.update(req.params.id, req.body);
+        }
+      })
+      .then((data) => {
+        if (data) {
+          return Post.findById(req.params.id);
+        }
+      })
+      .then((post) => {
+        res.json(post);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "The post information could not be retrieved",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
   }
 });
 
