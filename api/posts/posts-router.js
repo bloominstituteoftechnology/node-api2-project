3de6b1cce565ec9d7b335b0aea789b1ed427ef37
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-14 14:49:08 -0700

    routes added

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,28 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res)=>{
+
+});
+
+router.get('/:id', (req, res)=>{
+
+});
+
+router.post('', (req, res)=>{
+
+});
+
+router.put('/:id', (req, res)=>{
+
+});
+
+router.delete('/:id', (req, res)=>{
+
+});
+
+router.get('/:id/comments', (req, res)=>{
+
+});
+
 module.exports = router;
