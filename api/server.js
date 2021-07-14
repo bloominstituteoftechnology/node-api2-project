2259dcdfe882cc3412d70b694ab4687ebcd1af98
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-14 12:06:47 -0700

    added server, dynamic port

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const port = process.env.PORT || 8000;
+
+const server = express();
+server.use(express.json());
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it here
+
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, ()=>{
+    console.log(`listening on port: ${PORT}`);
+})
