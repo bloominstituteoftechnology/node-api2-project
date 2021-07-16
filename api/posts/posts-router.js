fd419962e6cc8169a2b3b6cf8e6d470465925712
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-15 23:24:08 -0700

    getting the hang of async await, 5 passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,30 +1,51 @@
 // implement your posts router here
-const express = require('express');
-
+const { response } = require("express");
+const express = require("express");
+const Post = require("./posts-model");
 const router = express.Router();
 
-router.get('/', (req, res)=>{
+router
+  .route("/")
+  .get(async (request, response) => {
+    try {
+      const posts = await Post.find();
+      response.json(posts);
+    } catch (error) {
+      response.status(500).json({
+        message: "The posts information could not be retrieved",
+        error: error.message,
+        stack: error.stack,
+      });
+    }
+  })
+  .post(async (request, response) => {
 
-});
+  })
 
-router.get('/:id', (req, res)=>{
+router
+  .route("/:id")
+  .get(async (request, response) => {
+    try {
+      const post = await Post.findById(request.params.id);
+      if (!post) {
+        response.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      } else {
+        response.json(post);
+      }
+    } catch (error) {
+      response.status(500).json({
+        message: "The post information could not be retrieved",
+        stack: error.stack,
+        error: error.message,
+      });
+    }
+  })
+  .post((request, response) => { })
+  .put((request, response) => { })
+  .delete((request, response) => { });
 
-});
-
-router.post('', (req, res)=>{
-
-});
-
-router.put('/:id', (req, res)=>{
-
-});
-
-router.delete('/:id', (req, res)=>{
-
-});
-
-router.get('/:id/comments', (req, res)=>{
-
-});
+router.get("/:id/comments", (request, response) => { });
 
 module.exports = router;
