299d471ccef321ae3bbceadd7987057e0e3eabc3
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-16 01:02:28 -0700

    all passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -107,7 +107,23 @@ router
   });
 
 router.get("/:id/comments", async(request, response) => {
-  
+  try {
+    const postID = await Post.findById(request.params.id);
+    if (!postID) {
+      response.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      const comments = await Post.findPostComments(request.params.id);
+    response.json(comments);
+    }
+  } catch (error) {
+    response.status(500).json({
+      message: "The comments information could not be retrieved",
+      stack: error.stack,
+      error: error.message,
+    });
+  }  
  });
 
 module.exports = router;
