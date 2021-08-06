ac38c2960e54da94ab21f597de716d5fa7c73c4d
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-08-05 20:42:21 -0500

    did basic set up for server and router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,5 @@
 // implement your posts router here
+const Posts = require('./posts-model');
+const express = require('express');
+const router = express.Router();
+
--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
-// require your posts router and connect it here
+const express = require('express')
+
+const server = express();
+
+// require your posts router and connect it here\
+
+const postsRouter = require('./posts/posts-router.js')
+
+server.use(express.json());
+server.use('/api/posts', postsRouter)
+
+module.exports = server;
