70d5901f6d4715d6054654bee5164c65e51ea94a
tdubs42 <contact-tdubs@protonmail.com>
2021-08-07 20:17:39 -0700

    mvp done

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
+const express = require( "express" );
+const server = express();
+server.use( express.json() );
+
 // require your posts router and connect it here
+const postsRouter = require( "./posts/posts-router.js" );
+server.use( "/api/posts", postsRouter );
+
+server.get( "/", ( req, res ) => {
+    res.send( `
+  <h2>Unit 4 Module 1 Assignment 2 API</h2>
+  <p>Welcome to the server</p>
+  ` );
+} );
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- data/migrations/20180401213248_add_posts_table.js
+++ data/migrations/20180401213248_add_posts_table.js
@@ -1,14 +1,16 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('posts', function(posts) {
-    posts.increments();
-
-    posts.string('title', 1024).notNullable();
-    posts.text('contents').notNullable();
-
-    posts.timestamps(true, true);
-  });
+exports.up = function ( knex ) {
+    return knex.schema.createTable( "posts", function ( posts ) {
+        posts.increments();
+        
+        posts.string( "title", 1024 )
+             .notNullable();
+        posts.text( "contents" )
+             .notNullable();
+        
+        posts.timestamps( true, true );
+    } );
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('posts');
+exports.down = function ( knex ) {
+    return knex.schema.dropTableIfExists( "posts" );
 };
