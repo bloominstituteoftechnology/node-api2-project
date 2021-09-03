eed261bf391c51fdea5296bc83ba94ce327da5f3
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-03 17:57:26 +0200

    Initializing

--- api/server.js
+++ api/server.js
@@ -1,2 +1,21 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express')
+const postsRouter = require('./posts/posts-router') 
+
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/posts', postsRouter)
+
+server.use('*', (req, res) => {
+    console.log('* default catch upp')
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+console.log('index.js console.log')
+
+const server = require('./api/server')
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
