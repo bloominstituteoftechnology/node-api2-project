e0cc81b0fc6bcf4a2afc36888f1711f1585bd361
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-05 01:12:45 +0200

    Put and delete finished, last get function next, 80%+

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -66,10 +66,64 @@ router.post('/', (req, res) => {
         })
     }
 })
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+    try {
+        //throw new Error('it :( did not work mate')
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            await Post.remove(req.params.id)
+            //console.log(post)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 router.put('/:id', (req, res) => {
+    const { title, contents } = req.body
+    if ( !title || !contents ) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.findById(req.params.id)
+        .then( stuff => {
+            //console.log(stuff)
+            if (!stuff) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else {
+                return Post.update(req.params.id, req.body)
+            }
+        })
+        .then(data => {
+            if(data){
+                return Post.findById(req.params.id) 
+            }
+        })
+        .then(post => {
+            if (post) {
+                res.json(post) 
+            } 
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be modified",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+
+    }
 
 })
 router.get('/:id/messages', (req, res) => { //or comments
