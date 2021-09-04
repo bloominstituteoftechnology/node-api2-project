2935abcd4ea4405acc0d84b510ba7ba8e262fbc9
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-05 01:28:17 +0200

    finalized, 17/17

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -126,8 +126,26 @@ router.put('/:id', (req, res) => {
     }
 
 })
-router.get('/:id/messages', (req, res) => { //or comments
-
+router.get('/:id/comments', async (req, res) => { //or comments
+    try {
+        //throw new Error('ka-bloinGGGGGGZ!')
+        const post = await Post.findById(req.params.id)
+        console.log('--->', post)
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        } 
+    }   catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message,
+            stack: err.stack
+        })
+    }
 })
 
 
