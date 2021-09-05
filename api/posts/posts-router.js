78d099fc6c0d4379e41eaf6a4e1e203202cf0d6c
Stan <stan2dor@gmail.com>
2021-09-05 16:49:42 -0700

    PUT - updated user

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -66,4 +66,39 @@ router.post("/", (req, res) => {
   }
 });
 
+router.put("/:id", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post",
+    });
+  } else {
+    Post.findById(req.params.id)
+      .then((posts) => {
+        if (!posts) {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist",
+          });
+        } else {
+          return Post.update(req.params.id, req.body);
+        }
+      })
+      .then((data) => {
+        if (data) {
+          return Post.findById(req.params.id);
+        }
+      })
+      .then((post) => {
+        res.json(post);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "The post information could not be retrieved",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
+
 module.exports = router;
