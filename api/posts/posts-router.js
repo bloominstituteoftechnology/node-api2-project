5f6f4bfd0543ed2ecd4fdc4d048cc4b74ee9c271
Stan <stan2dor@gmail.com>
2021-09-05 16:26:18 -0700

    POST - create new post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -41,4 +41,29 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const post = req.body;
+  if (!post.title || !post.contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post",
+    });
+  } else {
+    Post.insert(post)
+      .then(({ id }) => {
+        return Post.findById(id);
+      })
+      .then((newPost) => {
+        console.log(newPost);
+        res.status(201).json(newPost);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
+
 module.exports = router;
