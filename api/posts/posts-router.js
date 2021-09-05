9045b909432fc6eff9b9e19c44d09996a4ec346f
Stan <stan2dor@gmail.com>
2021-09-05 16:20:57 -0700

    GET - by ID all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -20,4 +20,25 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "The post information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
 module.exports = router;
