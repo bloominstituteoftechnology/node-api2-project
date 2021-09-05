46b3a7fc6faa3679dc08d2dcff6571877b943ffe
Stan <stan2dor@gmail.com>
2021-09-05 16:03:20 -0700

    project & server setup

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require("express");
+// const postsRouter = require("./posts/posts-router");
+
+const server = express();
+server.use(express.json());
+
+// server.use("/api/posts", postsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`
+  <h2> Stan tudor API </h2> 
+  <p> Welcome </p>`);
+});
+
+module.exports = server;
