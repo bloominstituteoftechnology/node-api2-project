db7b8d4d9c54e624f7b5a34c966e10ae31908d57
christina-yun <poncheeyun@yahoo.com>
2021-09-28 16:35:39 -0700

    Fixed the id to not be a string and all put tests are passing now

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -75,8 +75,25 @@ router.put('/:id', (req, res) => {
             message: "Please provide title and contents for the post"
         })
     } else {
-        Posts.update(parseInt(id), changes)
-        res.status(200).json(changes);
+        Posts.update(id, changes)
+        .then(postToUpdate => {
+            if(postToUpdate === 0) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else {
+                res.status(200).json({
+                    id:parseInt(id), 
+                    title:changes.title,
+                    contents:changes.contents
+                })
+            }
+        })
+        .catch(err=> {
+            res.status(500).json({
+                message: "The post information could not be modified" 
+            })
+        })
     }
 })
 
