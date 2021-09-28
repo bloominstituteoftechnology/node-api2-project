7ae8e7fdd30facdc388ea6d9039aecd4afab77b6
Pauline122112 <pauline.design007@gmail.com>
2021-09-28 13:15:45 -0500

    completed get method

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,23 @@
 // implement your posts router here
+const express = require("express")
+const Posts = require('./posts-model')
+const router = express.Router();
+
+router.get("/", (req, res) => {
+	Posts.find()
+		.then((posts) => {
+			res.json(posts);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				message: "The posts information could not be retrieved",
+				error: error.message,
+				stack: error.stack,
+			});
+		});
+});
+
+
+
+module.exports = router;
