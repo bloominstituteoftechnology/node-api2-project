c92104a8e0c665a731a2b2d430939d4f5e397dc0
christina-yun <poncheeyun@yahoo.com>
2021-09-28 14:50:32 -0700

    Finished router.post but does not pass test 7

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,58 +1,69 @@
-// implement your posts router here
-const express = require('express');
-const Posts = require('./posts-model');
+const express = require("express");
+const Posts = require("./posts-model");
 const router = express.Router();
 
 // [GET] /api/posts
-// Returns an array of all the post objects contained in the database
 router.get('/', (req, res) => {
-    Posts.find()
-        .then(resp => {
-            res.status(200).json(resp);
-        })
-        .catch(err => {
-            res.status(500).json({
-                message:"The posts information could not be retrieved"
-            })
-        })
+  Posts.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+      });
+    });
 });
 
 // [GET] /api/posts/:id
-// Returns the post object with the specified id
 router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    Posts.findById(id)
-        .then(post => {
-            if(!post){
-                res.status(404).json({
-                    message: "The post with the specified ID does not exist"
-                })
-            } else {
-                res.status(200).json(post)
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: "The post information could not be retrieved"
-            })
-        })
-})
+  const { id } = req.params;
+  Posts.findById(id)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The post information could not be retrieved",
+      });
+    });
+});
 
 // [POST] /api/posts
 // Creates a post using the information sent inside the request body and returns the newly created post object
+router.post('/', (req, res) => {
+    Posts.insert(req.body)
+    .then(newPost => {
+        const { title, contents } = req.body;
 
+        if(!title.trim() || !contents.trim()){
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            res.status(201).json({ id: newPost.id, title: title, contents: contents});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "There was an error while trying to save the post to the database"
+        })
+    })
+})
 
 // [PUT] /api/posts/:id
 // Updates the post with the specified id using data from the request body and returns the modified document, not the original
 
-
-// [DELETE] /api/posts/:id	
+// [DELETE] /api/posts/:id
 // Removes the post with the specified id and returns the deleted post object
-
 
 // [GET] /api/posts/:id/comments
 // Returns an array of all the comment objects associated with the post with the specified id
-
-
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,10 @@
-// implement your server here
-// require your posts router and connect it here
-const express = require('express');
-const postsRouter = require('./posts/posts-router');
+const express = require("express");
+
+const postsRouter = require("./posts/posts-router");
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/posts', postsRouter);
+server.use("/api/posts", postsRouter);
 
 module.exports = server;
