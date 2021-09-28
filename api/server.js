eb7dc6261a0d6a223c047e4e7c50f271514cd0cd
marciemaclean <85119043+marciemaclean@users.noreply.github.com>
2021-09-28 14:45:50 -0400

    github submit

--- api/server.js
+++ api/server.js
@@ -1,2 +1,20 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+//posts
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Lambda Shelter API</h>
+      <p>Welcome to the Lambda Shelter API</p>
+    `);
+  });
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+
+
+const server = require('./api/server');
+
+server.listen(4000, () => {
+
+console.log('server is running on port 4000')
+
+});
