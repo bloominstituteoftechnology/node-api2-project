eb293ce8221c66b7d2d610b315df9a5c8af434b2
Pauline122112 <pauline.design007@gmail.com>
2021-09-28 13:22:24 -0500

    finished getting id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,19 +4,42 @@ const Posts = require('./posts-model')
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	Posts.find()
-		.then((posts) => {
-			res.json(posts);
+	Posts.find(req.query)
+		.then(posts => {
+			res.status(200).json(posts);
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(500).json({
+				message: "The posts information could not be retrieved"
+			})
+		})
+})
+
+router.get("/:id", (req, res) => {
+	Posts.findById(req.params.id)
+		.then((post) => {
+			if (post) {
+				res.status(200).json(post);
+			} else {
+				res
+					.status(404)
+					.json({ 
+                        message: "The post with the specified ID does not exist" 
+                    });
+			}
 		})
 		.catch((error) => {
 			console.log(error);
 			res.status(500).json({
-				message: "The posts information could not be retrieved",
-				error: error.message,
-				stack: error.stack,
+				message: "The post information could not be retrieved",
 			});
 		});
 });
+
+
+
+
 
 
 
