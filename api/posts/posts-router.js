95c39126caf5720471e30b11a40d0797d5f157e1
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-09-28 15:09:52 -0500

    got server to connect with router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,37 @@
 // implement your posts router here
+const express = require("express");
+const Post = require("./posts-model");
+const router = express.Router();
+
+//get post-----------
+router.get("/", (req, res) => {
+  Post.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "cant retreive posts bruh, try again",
+      });
+    });
+});
+router.get("/:id ", (req, res) => {
+  const { id } = req.params;
+  Post.findById(id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "post not found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message:
+          "there's error with retreiving your post buddy, try another way",
+      });
+    });
+});
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+const postRouter = require("./posts/posts-router");
+const server = express();
+server.use(express.json()); // teaches express to read req.body as JSON
+server.use("/api/posts", postRouter);
+
+// server.get("/", (req, res) => {
+//   res.send(`
+//     <h2>api works </h>
+//   `);
+// });
+
+module.exports = server;
