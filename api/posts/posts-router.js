51ab1340bfbfaf082023a8e829a7035d1997fbbd
christina-yun <poncheeyun@yahoo.com>
2021-09-28 17:32:31 -0700

    get :id/comments is finished. One more test to go

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -96,7 +96,6 @@ router.put('/:id', (req, res) => {
 })
 
 // [DELETE] /api/posts/:id
-// Removes the post with the specified id and returns the deleted post object
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     
@@ -124,5 +123,26 @@ router.delete('/:id', (req, res) => {
 
 // [GET] /api/posts/:id/comments
 // Returns an array of all the comment objects associated with the post with the specified id
+router.get('/:id/comments', async (req, res) => {
+    try{
+        const postId = req.params.id;
+        const post = await Posts.findById(parseInt(postId));
+        const comments = await Posts.findPostComments(parseInt(postId));
+
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(comments);
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            message: "The comments information could not be retrieved" 
+        })
+    }
+})
+
 
 module.exports = router;
