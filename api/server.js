943f7ef907f7d3a5f160d362255db218da9ce52b
Marina Monsivais <minamonsi@me.com>
2021-10-01 16:00:47 -0500

    Server running

--- api/server.js
+++ api/server.js
@@ -1,2 +1,9 @@
 // implement your server here
+const express = require("express")
+const server = express();
 // require your posts router and connect it here
+
+server.use(express.json());
+
+
+module.exports = server;
