a56a384c7f2740b6038f306e8e41ac2a8c5423d0
Marina Monsivais <minamonsi@me.com>
2021-10-02 15:13:37 -0500

    WIP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -30,9 +30,26 @@ router.get("/:id", (req,res)=>{
     })
 })
 
-router.post("/", (req,res)=>{
-    
+router.post("/", (req,res)=>{ 
+    const {title, contents} = req.body
+    if(!title || !contents){
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    }else{ 
+        Post.insert({title, contents})
+        .then(({id}) => {
+            return Post.findById(id)
+        })
+        .then(post => {
+            res.status(201).json(post);
+        }) 
+        .catch(err =>{
+            res.status(500).json({ message: "There was an error while saving the post to the database" ,
+            err: err.message
+        })
+        }) 
+    }
 })
+  
 
 router.put("/:id", (req,res)=>{
     
