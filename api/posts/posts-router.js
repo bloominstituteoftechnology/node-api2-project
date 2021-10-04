3e999e26e8fdd021b4edfb0ad89e50bba9f851b7
Marina Monsivais <minamonsi@me.com>
2021-10-03 22:48:37 -0500

    Endpoints completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -91,7 +91,21 @@ router.delete("/:id", async(req,res)=>{
 })
 
 router.get("/:id/comments", (req,res)=>{
-    
+    Posts.findCommentById(req.params.id)
+    if(!id){
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+    }else{
+        Posts.findPostComments(id)
+        .then(comments => {
+            res.status(200).json(comments)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "The comments information could not be retrieved",
+            });
+        })
+    }
 })
 
 module.exports = router;
