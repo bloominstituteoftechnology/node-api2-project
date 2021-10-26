34189ad697ff34737565c5b8c8789842a44a043f
jimlemoine <jlemoine007@gmail.com>
2021-10-26 12:30:34 -0700

    added server to index js

--- api/server.js
+++ api/server.js
@@ -1,2 +1,9 @@
 // implement your server here
+const express = require('express');
 // require your posts router and connect it here
+const postsRouter = require('./posts/posts-router');
+const server = express();
+
+server.use(express.json());
+
+module.exports = server;
