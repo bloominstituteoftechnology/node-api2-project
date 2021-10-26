eb19ee70056f1a0c121a0312899da4a20be2d569
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-10-26 16:24:13 -0600

    done and done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -95,4 +95,21 @@ router.put('/:id', (req, res) => {
     
 })
 
+router.get('/:id/comments', async (req, res)=> {
+    try{
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+
+        }else {
+        const comments = await Post.findPostComments(req.params.id)
+        res.json(comments)
+        }
+
+    }catch (err) {
+        res.status(500).json({ message: "The comments information could not be retrieved" })
+
+    }
+})
+
 module.exports = router
