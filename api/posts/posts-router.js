1e002ba24eb6e0839846babf4cf960c27ed728b4
Sebastian <sebas6292@gmail.com>
2021-10-26 16:22:10 -0700

    comments was a success

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -118,9 +118,18 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
-router.get('/id/comments', async (req, res) => {
+router.get('/:id/comments', async (req, res) => {
+    const { id } = req.params
     try {
-
+        const post = await Post.findById(id)
+        if (!post) { 
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else { 
+            const messages = await Post.findPostComments(req.params.id)
+            res.status(200).json(messages)
+        }
     }catch (err) {
         res.status(500).json({
             message: "The posts information could not be retrieved",
