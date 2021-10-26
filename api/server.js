d3c6014f8f94bf261d4b12fcb89c71478b425270
Adam K <krueng@gmail.com>
2021-10-26 14:58:05 -0500

    Initiated server on port 5000

--- api/server.js
+++ api/server.js
@@ -1,2 +1,7 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // require your server and launch it here
+const server = require('./api/server');
+server.listen (5000, () =>{
+    console.log('Server is alive on http://localhost:5000');
+})
