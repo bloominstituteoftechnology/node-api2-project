68045c23684eabb990365576db02b02bae7ff0b3
Adam K <krueng@gmail.com>
2021-10-26 16:15:56 -0500

    [GET] /api/posts/:id. Returns the post object with the specified id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -8,7 +8,24 @@ router.get('/', (req, res) => {
         .then(posts => {
             res.status(200).json(posts);
         })
-        .catch(err =>{
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'Error fetching posts'
+            });
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: 'Post does not exist' })
+            }
+        })
+        .catch(err => {
             console.log(err);
             res.status(500).json({
                 message: 'Error fetching posts'
