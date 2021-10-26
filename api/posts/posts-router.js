bf907dc92486e6ae6c67ff473c671c71ffced4bc
Sebastian <sebas6292@gmail.com>
2021-10-26 13:33:06 -0700

    port is listening and router is hooked correctly

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,30 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+    
+})
+
+router.post('/', (req, res) => {
+    
+})
+
+router.put('/id', (req, res) => {
+    
+})
+
+router.delete('/id', (req, res) => {
+    
+})
+
+router.get('/id/comments', (req, res) => {
+    
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
+const express = require('express')
+const postsRouter = require('./posts/posts-router.js')
+const server = express()
+server.use(express.json())
+
+server.use('/api/posts', postsRouter)
 // require your posts router and connect it here
+
+server.use('*', (req, res) => { 
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+module.exports = server; 
