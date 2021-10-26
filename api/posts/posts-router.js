3aa1db3bd699a45b1a680dee246c8ffe85a24b3b
Sebastian <sebas6292@gmail.com>
2021-10-26 14:35:26 -0700

    post was a success

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -17,8 +17,9 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
+    const { id } = req.params
     try { 
-        const post = await Post.findById(req.params.id)
+        const post = await Post.findById(id)
         if (!post) {
             res.status(404).json({
                 message: "The post with the specified ID does not exist",
@@ -37,7 +38,27 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    
+    const { title, contents} = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post",
+        })
+    } else { 
+       Post.insert({ title, contents})
+         .then(({id}) => { //destruct the id so that you can grab not just the id but the title etc.
+                return Post.findById(id)
+         })
+         .then(post => {
+             res.status(201).json(post)
+         })
+         .catch(err => { 
+            res.status(500).json({
+            message: "There was an error while saving the post to the database",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+    } 
 })
 
 router.put('/:id', (req, res) => {
