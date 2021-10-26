9c16e69358368f2e6775e6cc36b8553aa6a1215f
Sebastian <sebas6292@gmail.com>
2021-10-26 14:07:16 -0700

    grabing posts and its IDs

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,13 +1,38 @@
-// implement your posts router here
 const express = require('express')
 const Post = require('./posts-model')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-
+    Post.find()
+    .then(post => { 
+        res.json(post)
+    })
+    .catch(err => { 
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try { 
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else { 
+            res.status(200).json(post)
+        }
+    } catch (err) { 
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
     
 })
 
@@ -15,11 +40,11 @@ router.post('/', (req, res) => {
     
 })
 
-router.put('/id', (req, res) => {
+router.put('/:id', (req, res) => {
     
 })
 
-router.delete('/id', (req, res) => {
+router.delete('/:id', (req, res) => {
     
 })
 
