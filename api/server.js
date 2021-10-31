860e0d164575c233a350bc9ab1d1f74518a50d21
Greg <gbrewton1980@gmail.com>
2021-10-31 15:55:13 -0600

    build(Scaffold): dummy endpoints
    creates dummy endpoints

--- api/server.js
+++ api/server.js
@@ -1,2 +1,33 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const server = express();
+
+const POST_URL = "/api/posts";
+
+server.get(POST_URL, (req, res) => {
+  res.send(`GET response ${POST_URL}`);
+});
+
+server.get(`${POST_URL}/:id`, (req, res) => {
+  res.send(`GET response ${POST_URL}/:id`);
+});
+
+server.post(POST_URL, (req, res) => {
+  res.send(`POST response ${POST_URL}`);
+});
+
+server.put(`${POST_URL}/:id`, (req, res) => {
+  res.send(`PUT response ${POST_URL}/:id`);
+});
+
+server.delete(`${POST_URL}/:id`, (req, res) => {
+  res.send(`DELETE response ${POST_URL}/:id`);
+});
+
+server.get(`${POST_URL}/:id/comments`, (req, res) => {
+  res.send(`GET response ${POST_URL}/:id/comments`);
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+server.listen(5000, () => {
+  console.log("server listening on port 5000");
+});
