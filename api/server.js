94b97b9921c1acdf1d58d92025166889ff86f7e9
Greg <gbrewton1980@gmail.com>
2021-10-31 16:15:46 -0600

    + HTTP response codes

--- api/server.js
+++ api/server.js
@@ -7,27 +7,27 @@ const server = express();
 const POST_URL = "/api/posts";
 
 server.get(POST_URL, (req, res) => {
-  res.send(`GET response ${POST_URL}`);
+  res.status(200).send(`GET response ${POST_URL}`);
 });
 
 server.get(`${POST_URL}/:id`, (req, res) => {
-  res.send(`GET response ${POST_URL}/:id`);
+  res.status(200).send(`GET response ${POST_URL}/:id`);
 });
 
 server.post(POST_URL, (req, res) => {
-  res.send(`POST response ${POST_URL}`);
+  res.status(201).send(`POST response ${POST_URL}`);
 });
 
 server.put(`${POST_URL}/:id`, (req, res) => {
-  res.send(`PUT response ${POST_URL}/:id`);
+  res.status(200).send(`PUT response ${POST_URL}/:id`);
 });
 
 server.delete(`${POST_URL}/:id`, (req, res) => {
-  res.send(`DELETE response ${POST_URL}/:id`);
+  res.status(200).send(`DELETE response ${POST_URL}/:id`);
 });
 
 server.get(`${POST_URL}/:id/comments`, (req, res) => {
-  res.send(`GET response ${POST_URL}/:id/comments`);
+  res.status(200).send(`GET response ${POST_URL}/:id/comments`);
 });
 
 module.exports = server;
