f99310913606b612edfdffdbe1df2ffcd35b243a
Greg <gbrewton1980@gmail.com>
2021-10-31 19:39:43 -0600

    build(Task 3): POST
    Create new post with error handling

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,30 +5,57 @@ const server = express();
 server.use(express.json());
 const model = require("./posts-model");
 
-server.get("/", async (req, res) => {
-  try {
-    const posts = await model.find();
-    res.status(200).send(posts);
-  } catch (e) {
-    res
-      .status(500)
-      .send({ message: "The posts information could not be retrieved" });
-  }
+server.get("/", (req, res) => {
+  model
+    .find()
+    .then((posts) => res.send(posts))
+    .catch(() => {
+      res
+        .status(500)
+        .send({ message: "The posts information could not be retrieved" });
+    });
 });
 
-server.get(`/:id`, async (req, res) => {
-  try {
-    const id = req.params.id;
-    const post = await model.findById(id);
-    res.status(200).send(post);
-  } catch (e) {
-    res
-      .status(500)
-      .send({ message: "The post with the specified ID does not exist" });
-  }
+server.get(`/:id`, (req, res) => {
+  const id = req.params.id;
+  model
+    .findById(id)
+    .then((post) => {
+      if (post) {
+        res.status(200).send(post);
+      } else {
+        res
+          .status(400)
+          .send({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .send({ message: "The post information could not be retrieved" });
+    });
 });
 
 server.post("/", async (req, res) => {
+  const post = req.body;
+  if (!post.title || !post.contents) {
+    res
+      .status(400)
+      .send({ message: "Please provide title and contents for the post" });
+  } else {
+    model
+      .insert(post)
+      .then((newPost) => res.status(201).send(newPost))
+      .catch(() => {
+        res.status(500).send({
+          message: "There was an error while saving the post to the database",
+        });
+      });
+  }
+});
+
+server.put(`/:id`, (req, res) => {
+  const id = req.params.id;
   if (!req.body.title || !req.body.contents) {
     res
       .status(400)
@@ -36,34 +63,36 @@ server.post("/", async (req, res) => {
   } else {
     const post = req.body;
     try {
-      model.insert(post);
-      res.status(201).send(post)
+      model.update(id, post);
+      res.status(200).send(post);
     } catch (err) {
       res.status(500).send({
-        message: "There was an error while saving the post to the database",
+        message: { message: "The post information could not be modified" },
       });
     }
   }
-});
-
-server.put(`/:id`, (req, res) => {
-  res.status(200).send(`PUT response /:id`);
 });
 
 server.delete(`/:id`, (req, res) => {
   const id = req.params.id;
   try {
     model.remove(id);
-    res.status(200).send('record deleted');
+    res.status(200).send("record deleted");
   } catch (e) {
-    res
-      .status(500)
-      .send({ message: "The post could not be removed" });
+    res.status(500).send({ message: "The post could not be removed" });
   }
 });
 
 server.get(`/:id/comments`, (req, res) => {
-  res.status(200).send(`GET response /:id/comments`);
+  const id = req.params.id;
+  try {
+    const comments = model.findPostComments(id);
+    res.status(200).send(comments);
+  } catch (e) {
+    res
+      .status(500)
+      .send({ message: "The comments information could not be retrieved" });
+  }
 });
 
 module.exports = server;
