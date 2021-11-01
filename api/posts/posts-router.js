cd2fbf0c5654716fa2f4514094c22d0c5b522e56
Greg <gbrewton1980@gmail.com>
2021-10-31 19:10:55 -0600

    create new post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,29 +2,48 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
 const model = require("./posts-model");
 
 server.get("/", async (req, res) => {
   try {
     const posts = await model.find();
     res.status(200).send(posts);
-  } catch (err) {
-    res.status(500).send({ error: err });
+  } catch (e) {
+    res
+      .status(500)
+      .send({ message: "The posts information could not be retrieved" });
   }
 });
 
 server.get(`/:id`, async (req, res) => {
   try {
     const id = req.params.id;
-    const post = await model.findById(id)
+    const post = await model.findById(id);
     res.status(200).send(post);
-  } catch (err) {
-    res.status(500).send({error: err})
+  } catch (e) {
+    res
+      .status(500)
+      .send({ message: "The post with the specified ID does not exist" });
   }
 });
 
-server.post("/", (req, res) => {
-  res.status(201).send(`POST response `);
+server.post("/", async (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .send({ message: "Please provide title and contents for the post" });
+  } else {
+    const post = req.body;
+    try {
+      model.insert(post);
+      res.status(201).send(post)
+    } catch (err) {
+      res.status(500).send({
+        message: "There was an error while saving the post to the database",
+      });
+    }
+  }
 });
 
 server.put(`/:id`, (req, res) => {
@@ -32,7 +51,15 @@ server.put(`/:id`, (req, res) => {
 });
 
 server.delete(`/:id`, (req, res) => {
-  res.status(200).send(`DELETE response /:id`);
+  const id = req.params.id;
+  try {
+    model.remove(id);
+    res.status(200).send('record deleted');
+  } catch (e) {
+    res
+      .status(500)
+      .send({ message: "The post could not be removed" });
+  }
 });
 
 server.get(`/:id/comments`, (req, res) => {
