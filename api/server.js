8e73c9181a8723b2a60b9e51135a6ff39519a163
Craig Barcinas <cbarcinas@live.com>
2021-11-01 14:46:28 -0700

    added vunerability fixes via helmet

--- api/server.js
+++ api/server.js
@@ -1,2 +1,28 @@
 // implement your server here
+const express = require("express"); // this gives us our http methods
+
+const cors = require("cors");
+const helmet = require("helmet"); // adds more vunerability fixes
+const morgan = require("morgan");
+
+const server = express();
+
+server.use(express.json()); // allows us to send JSON in the body of the req
+server.use(cors());
+server.use(helmet());
+server.use(morgan("dev"));
+
+const currentTime = new Date().toLocaleTimeString();
+
+server.get("/status", (req, res) => {
+  //   res.send("hello from node");
+  res.status(200).json({
+    status: 200,
+    message: `Server is up and running at ${currentTime}`,
+    author: "Github: @cbarcinas",
+  });
+});
+
 // require your posts router and connect it here
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
+
 // require your server and launch it here
+
 const server = require("./api/server");
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-// start server
-server.listen(port, () => {
-  console.log("Listening on port: ", port);
+// // this should log our .env file port number
+// console.log(process.env.PORT);
+
+// start server listener
+server.listen(PORT, () => {
+  console.log(
+    `Server is running on port: ${PORT} in ${process.env.NODE_ENV} mode`
+  );
 });
