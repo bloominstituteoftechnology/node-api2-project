cf68cd0a6e3e1fb0b4606e5e54f824ebee13813c
Craig Barcinas <cbarcinas@live.com>
2021-10-31 23:21:36 -0700

    created index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+const port = 5000;
+
+// start server
+server.listen(port, () => {
+  console.log("Listening on port: ", port);
+});
