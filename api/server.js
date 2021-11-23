cc0252dc48c686a92942a9f4528a118cad5b5490
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-22 19:43:34 -0800

    completed project again

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+const postsRouter = require('./posts/posts-router')
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/posts', postsRouter)
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'DUN DUN DUN not found'
+    })
+})
+
+module.exports = server
