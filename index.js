c2a33a380eee4b93a5eb9b7081ad72c7b90604bb
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-06 18:31:15 -0600

    set up server and post router

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./api/server.js')
+
+const port = 9000
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
+})
