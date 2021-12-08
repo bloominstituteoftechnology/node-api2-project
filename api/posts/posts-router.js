bd3be278b70f158bf552c514f8d1722c54f4e8a3
LirongCui <lirongcoding@gmail.com>
2021-12-08 15:12:26 -0800

    delete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
             res.status(500).json({
                 message: "The posts information could not be retrieved",
                 err: err.message,
-                statck: err.statck
+                stack: err.stack
             })
         })
 })
@@ -33,14 +33,53 @@ router.get('/:id', async (req, res) => {
         res.status(500).json({ 
             message: "The post information could not be retrieved",
             err: err.message,
-            statck: err.statck
+            stack: err.stack
         })
     }
 })
-router.post('/', (req, res) => {
 
+router.post('/', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.insert({ title, contents })
+            .then(({id}) => {
+                return Post.findById(id)
+            })
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database",
+                    err: err.message,
+                    stack: err.stack
+                })
+            })
+    }
 })
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if (!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({ 
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 
 })
 router.put('/:id', (req, res) => {
