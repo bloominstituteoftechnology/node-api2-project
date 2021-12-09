324aabeaebdb8417cc856c70b2775b13eb45a1f8
LirongCui <lirongcoding@gmail.com>
2021-12-09 01:34:48 -0800

    mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -120,7 +120,25 @@ router.put('/:id', (req, res) => {
     }
 })
 
-router.get('/:id/message', (req, res) => {
-
+router.get('/:id/comments', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+            if (!post) {
+                res.status(404).json({
+                    message:"The post with the specified ID does not exist"
+                })
+            } else {
+                const message = await Post.findPostComments(req.params.id)
+                res.json(message)
+            }
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error while saving the post to the database",
+            err: err.message,
+            stack: err.stack
+        })
+    }
+    
 })
+
 module.exports = router
