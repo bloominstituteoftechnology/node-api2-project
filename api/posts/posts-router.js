47d38a7f6609c0c131345784b5946252b3986e3a
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-16 19:43:09 -0800

    get post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,4 +16,21 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        }else{
+            res.json(post)
+        }
+    }catch(err){
+        res.status(500).json({
+            message: 'The post information could not be retrieved'
+        })
+    }
+})
+
 module.exports = router
