8e0fe0a7d0750c2dc945c81cc8d18a9ee13b4cfb
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-16 19:38:12 -0800

    get posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,5 +4,16 @@ const Post = require('./posts-model')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Post.find()
+    .then(posts => {
+        res.json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved'
+        })
+    })
+})
 
 module.exports = router
