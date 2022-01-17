5209b5fb32ccdee79cd60e5c99053e456a125f71
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-16 19:53:13 -0800

    post posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -33,4 +33,26 @@ router.get('/:id', async(req, res) => {
     }
 })
 
+router.post('/', (req, res) => {
+    const { title, contents } = req.body
+    if(!title || !contents) {
+        res.status(400).json({
+            message: 'Please provide title and contents for the post'
+        })
+    } else {
+        Post.insert({ title, contents })
+        .then(({ id }) => {
+            return Post.findById(id)
+        })
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database'
+            })
+        })
+    }
+})
+
 module.exports = router
