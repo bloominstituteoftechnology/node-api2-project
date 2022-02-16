1620a9ec210b017c60f6c82065674260be8995a4
Amanda Burtness <amandaburtness@gmail.com>
2022-02-15 21:54:47 -0600

    completed mvp

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express')
+
+const server = express()
+const postsRouter = require('./posts/posts-router')
+
+server.use(express.json())
+server.use('/api/posts', postsRouter)
+
+
+server.get('/', (req, res) => {
+  res.send(`welcome to this api`)
+})
+
+module.exports = server
